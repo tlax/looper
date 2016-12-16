dafc58643c8a9a97dcb641718abca68447f05c0c
vauxhall <zero.griffin@gmail.com>
2016-12-16 08:59:07 -0600

    right to left and right to right

--- looper/Extensions/ExtensionNSLayoutConstraint.swift
+++ looper/Extensions/ExtensionNSLayoutConstraint.swift
@@ -15,4 +15,46 @@ extension NSLayoutConstraint
         
         return constraint
     }
+    
+    class func leftToRight(view:UIView, toView:UIView, constant:CGFloat) -> NSLayoutConstraint
+    {
+        let constraint:NSLayoutConstraint = NSLayoutConstraint(
+            item:view,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:toView,
+            attribute:NSLayoutAttribute.right,
+            multiplier:1,
+            constant:constant)
+        
+        return constraint
+    }
+    
+    class func rightToRight(view:UIView, toView:UIView, constant:CGFloat) -> NSLayoutConstraint
+    {
+        let constraint:NSLayoutConstraint = NSLayoutConstraint(
+            item:view,
+            attribute:NSLayoutAttribute.right,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:toView,
+            attribute:NSLayoutAttribute.right,
+            multiplier:1,
+            constant:constant)
+        
+        return constraint
+    }
+    
+    class func rightToLeft(view:UIView, toView:UIView, constant:CGFloat) -> NSLayoutConstraint
+    {
+        let constraint:NSLayoutConstraint = NSLayoutConstraint(
+            item:view,
+            attribute:NSLayoutAttribute.right,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:toView,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:constant)
+        
+        return constraint
+    }
 }
