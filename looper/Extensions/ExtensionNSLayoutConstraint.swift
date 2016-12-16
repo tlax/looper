a9a5cd77c5cf417604881a4fc565959c0c824f27
vauxhall <zero.griffin@gmail.com>
2016-12-16 08:58:02 -0600

    left to left

--- looper/Extensions/ExtensionNSLayoutConstraint.swift
+++ looper/Extensions/ExtensionNSLayoutConstraint.swift
@@ -2,8 +2,17 @@ import UIKit
 
 extension NSLayoutConstraint
 {
-    class func left(view:UIView, toView:UIView, amount:CGFloat) -> NSLayoutConstraint
+    class func leftToLeft(view:UIView, toView:UIView, constant:CGFloat) -> NSLayoutConstraint
     {
+        let constraint:NSLayoutConstraint = NSLayoutConstraint(
+            item:view,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:toView,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:constant)
         
+        return constraint
     }
 }
