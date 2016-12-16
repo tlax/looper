5e3b8632383321901781c2ea172a047d08b65ab0
vauxhall <zero.griffin@gmail.com>
2016-12-16 08:55:43 -0600

    factory layout constraint

--- looper/Extensions/ExtensionNSLayoutConstraint.swift
+++ looper/Extensions/ExtensionNSLayoutConstraint.swift
@@ -2,5 +2,8 @@ import UIKit
 
 extension NSLayoutConstraint
 {
-    class func left(view:UIView, toView:UIView, )
+    class func left(view:UIView, toView:UIView, amount:CGFloat) -> NSLayoutConstraint
+    {
+        
+    }
 }
