c64a21330f1d67f03b110acb010c1e32b64e32f7
vauxhall <zero.griffin@gmail.com>
2016-12-16 09:58:09 -0600

    control collection

--- looper/View/Home/VHomeControl.swift
+++ looper/View/Home/VHomeControl.swift
@@ -4,14 +4,22 @@ class VHomeControl:UIView, UICollectionViewDelegate, UICollectionViewDataSource,
 {
     private weak var controller:CHome!
     private weak var collectionView:UICollectionView!
+    let model:MHomeControl
     
-    convenience init(controller:CHome)
+    init(controller:CHome)
     {
-        self.init()
+        model = MHomeControl()
+        
+        super.init(frame:CGRect.zero)
         clipsToBounds = true
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor.clear
         self.controller = controller
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
     }
     
     //MARK: collectionView delegate
@@ -19,5 +27,17 @@ class VHomeControl:UIView, UICollectionViewDelegate, UICollectionViewDataSource,
     func numberOfSections(in collectionView:UICollectionView) -> Int
     {
         return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = model.items.count
+        
+        return count
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let cell
     }
 }
