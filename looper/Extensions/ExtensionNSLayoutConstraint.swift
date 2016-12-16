9f1932a366716a608b812ead687c4e9c2e9aae12
vauxhall <zero.griffin@gmail.com>
2016-12-16 09:00:43 -0600

    top to top and top to bottom

--- looper/Extensions/ExtensionNSLayoutConstraint.swift
+++ looper/Extensions/ExtensionNSLayoutConstraint.swift
@@ -2,6 +2,34 @@ import UIKit
 
 extension NSLayoutConstraint
 {
+    class func topToTop(view:UIView, toView:UIView, constant:CGFloat) -> NSLayoutConstraint
+    {
+        let constraint:NSLayoutConstraint = NSLayoutConstraint(
+            item:view,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:toView,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:constant)
+        
+        return constraint
+    }
+    
+    class func topToBottom(view:UIView, toView:UIView, constant:CGFloat) -> NSLayoutConstraint
+    {
+        let constraint:NSLayoutConstraint = NSLayoutConstraint(
+            item:view,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:toView,
+            attribute:NSLayoutAttribute.bottom,
+            multiplier:1,
+            constant:constant)
+        
+        return constraint
+    }
+    
     class func leftToLeft(view:UIView, toView:UIView, constant:CGFloat) -> NSLayoutConstraint
     {
         let constraint:NSLayoutConstraint = NSLayoutConstraint(
