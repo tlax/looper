b57579c140c39fd0a209c30bdd5695edc21e664a
vauxhall <zero.griffin@gmail.com>
2016-12-19 10:44:57 -0600

    collectionview in timeline

--- looper/View/Home/Timeline/VHomeTimeline.swift
+++ looper/View/Home/Timeline/VHomeTimeline.swift
@@ -13,6 +13,26 @@ class VHomeTimeline:UIView, UICollectionViewDelegate, UICollectionViewDataSource
         backgroundColor = UIColor.clear
         self.controller = controller
         
-        let flow:
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize.zero
+        flow.minimumLineSpacing = 0
+        flow.minimumInteritemSpacing = 0
+        flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+        
+        let collectionView:UICollectionView = UICollectionView(
+            frame:CGRect.zero,
+            collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellWithReuseIdentifier: <#T##String#>)
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
     }
 }
