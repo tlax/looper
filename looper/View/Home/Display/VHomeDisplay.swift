734c727b005c72818c74e3185a540202af2a698e
vauxhall <zero.griffin@gmail.com>
2016-12-19 15:47:03 -0600

    dispay displayFrame()

--- looper/View/Home/Display/VHomeDisplay.swift
+++ looper/View/Home/Display/VHomeDisplay.swift
@@ -3,6 +3,7 @@ import UIKit
 class VHomeDisplay:UIView
 {
     private weak var controller:CHome!
+    private weak var imageView:UIImageView!
     
     convenience init(controller:CHome)
     {
@@ -11,5 +12,41 @@ class VHomeDisplay:UIView
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor.clear
         self.controller = controller
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        imageView.clipsToBounds = true
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        let layoutImageTop:NSLayoutConstraint = NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self)
+        let layoutImageBottom:NSLayoutConstraint = NSLayoutConstraint.bottomToBottom(
+            view:imageView,
+            toView:self)
+        let layoutImageLeft:NSLayoutConstraint = NSLayoutConstraint.leftToLeft(
+            view:imageView,
+            toView:self)
+        let layoutImageRight:NSLayoutConstraint = NSLayoutConstraint.rightToRight(
+            view:imageView,
+            toView:self)
+        
+        addConstraints([
+            layoutImageTop,
+            layoutImageBottom,
+            layoutImageLeft,
+            layoutImageRight])
+    }
+    
+    //MARK: public
+    
+    func displayFrame(image:UIImage)
+    {
+        imageView.stopAnimating()
+        imageView.image = image
     }
 }
