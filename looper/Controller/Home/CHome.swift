c163208057a541974276ef3de051805fc0a3a12b
vauxhall <zero.griffin@gmail.com>
2016-12-19 17:26:20 -0600

    controller state

--- looper/Controller/Home/CHome.swift
+++ looper/Controller/Home/CHome.swift
@@ -2,12 +2,22 @@ import UIKit
 
 class CHome:CController
 {
+    enum State
+    {
+        case standBy
+        case rendering
+        case frame
+        case playing
+    }
+    
     weak var viewHome:VHome!
     let modelImage:MHomeImage
+    var state:State
     
     override init()
     {
         modelImage = MHomeImage()
+        state = State.standBy
         
         super.init()
     }
