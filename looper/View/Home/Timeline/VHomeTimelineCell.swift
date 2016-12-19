61dcd2fa6e35bcbfa318e3de1983212b681c4686
vauxhall <zero.griffin@gmail.com>
2016-12-19 11:10:31 -0600

    timeline cell

---
+++ looper/View/Home/Timeline/VHomeTimelineCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class VHomeTimelineCell:UICollectionViewCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+}
