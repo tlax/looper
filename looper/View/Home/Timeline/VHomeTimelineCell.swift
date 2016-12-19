d7673451c57a9dc06ceefd28baf38b8e61dfcfeb
vauxhall <zero.griffin@gmail.com>
2016-12-19 11:44:45 -0600

    imageView constraints

--- looper/View/Home/Timeline/VHomeTimelineCell.swift
+++ looper/View/Home/Timeline/VHomeTimelineCell.swift
@@ -2,11 +2,41 @@ import UIKit
 
 class VHomeTimelineCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         backgroundColor = UIColor.white
         clipsToBounds =  true
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        let layoutImageTop:NSLayoutConstraint = NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self)
+        let layoutImageBottom:NSLayoutConstraint = NSLayoutConstraint.bottomToBottom(
+            view:imageView,
+            toView:self)
+        let layoutImageLeft:NSLayoutConstraint = NSLayoutConstraint.leftToLeft(
+            view:imageView,
+            toView:self)
+        let layoutImageRight:NSLayoutConstraint = NSLayoutConstraint.rightToRight(
+            view:imageView,
+            toView:self)
+        
+        addConstraints([
+            layoutImageTop,
+            layoutImageBottom,
+            layoutImageLeft,
+            layoutImageRight])
     }
     
     required init?(coder:NSCoder)
@@ -18,5 +48,6 @@ class VHomeTimelineCell:UICollectionViewCell
     
     func config(model:MHomeImageSequenceItem)
     {
+        imageView.image = model.image
     }
 }
