17e8bc19552b59360424c75c75b89d23f2dd5457
vauxhall <zero.griffin@gmail.com>
2016-12-19 11:55:38 -0600

    generate sequence

--- looper/Model/Home/Image/MHomeImage.swift
+++ looper/Model/Home/Image/MHomeImage.swift
@@ -2,6 +2,7 @@ import Foundation
 
 class MHomeImage
 {
+    var generatedSequence:MHomeImageSequence?
     private(set) var sequences:[MHomeImageSequence]
     
     init()
@@ -9,14 +10,38 @@ class MHomeImage
         sequences = []
     }
     
+    //MARK: private
+    
+    private func asyncGenerateSequence()
+    {
+        NotificationCenter.default.post(
+            name:Notification.imagesUpdated,
+            object:nil)
+    }
+    
     //MARK: public
     
     func add(sequence:MHomeImageSequence)
     {
+        generatedSequence = nil
         sequences.append(sequence)
         
         NotificationCenter.default.post(
             name:Notification.imagesUpdated,
             object:nil)
+    }
+    
+    func generateSequence() -> MHomeImageSequence?
+    {
+        guard
+        
+            let generatedSequence:MHomeImageSequence = self.generatedSequence
+        
+        else
+        {
+            return nil
+        }
+        
+        return generatedSequence
     }
 }
