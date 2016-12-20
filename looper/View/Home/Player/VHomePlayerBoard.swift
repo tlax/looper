002d501188184e19bd1d839839791f7273a5256a
vauxhall <zero.griffin@gmail.com>
2016-12-20 10:32:38 -0600

    view player

--- looper/View/Home/Player/VHomePlayerBoard.swift
+++ looper/View/Home/Player/VHomePlayerBoard.swift
@@ -4,6 +4,9 @@ class VHomePlayerBoard:UIView
 {
     private weak var controller:CHome!
     private weak var buttonMain:VHomePlayerBoardMain!
+    private weak var layoutMainLeft:NSLayoutConstraint!
+    private let kMainWidth:CGFloat = 70
+    private let kButtonsHeight:CGFloat = 60
     
     convenience init(controller:CHome)
     {
@@ -15,7 +18,37 @@ class VHomePlayerBoard:UIView
         
         let buttonMain:VHomePlayerBoardMain = VHomePlayerBoardMain(
             controller:controller)
+        self.buttonMain = buttonMain
         
         addSubview(buttonMain)
+        
+        let layoutMainHeight:NSLayoutConstraint = NSLayoutConstraint.height(
+            view:buttonMain,
+            constant:kButtonsHeight)
+        let layoutMainBottom:NSLayoutConstraint = NSLayoutConstraint.bottomToBottom(
+            view:buttonMain,
+            toView:self)
+        layoutMainLeft = NSLayoutConstraint.leftToLeft(
+            view:buttonMain,
+            toView:self)
+        let layoutMainWidth:NSLayoutConstraint = NSLayoutConstraint.width(
+            view:buttonMain,
+            constant:kMainWidth)
+        
+        addConstraints([
+            layoutMainHeight,
+            layoutMainBottom,
+            layoutMainLeft,
+            layoutMainWidth])
+    }
+    
+    override func layoutSubviews()
+    {
+        let totalWidth:CGFloat = bounds.maxX
+        let mainRemain:CGFloat = totalWidth - kMainWidth
+        let mainMargin:CGFloat = mainRemain / 2.0
+        layoutMainLeft.constant = mainMargin
+        
+        super.layoutSubviews()
     }
 }
