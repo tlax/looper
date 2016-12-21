4fd3d29acba6053567608aa0ef5de8562f6a99b6
vauxhall <zero.griffin@gmail.com>
2016-12-21 12:18:03 -0600

    frames 45

--- looper/Model/Home/Frames/MHomeFramesItem45.swift
+++ looper/Model/Home/Frames/MHomeFramesItem45.swift
@@ -1,9 +1,21 @@
-//
-//  MHomeFramesItem45.swift
-//  looper
-//
-//  Created by zero on 12/21/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MHomeFramesItem45:MHomeFramesItem
+{
+    private let kFramesPerSecond:TimeInterval = 45
+    
+    override init()
+    {
+        let timeInterval:TimeInterval = 1 / kFramesPerSecond
+        let name:String = "\(timeInterval)"
+        
+        super.init(
+            timeInterval:timeInterval,
+            name:name)
+    }
+    
+    override init(timeInterval:TimeInterval, name:String)
+    {
+        fatalError()
+    }
+}
