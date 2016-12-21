cacea1001ae3f3cdf048cba0d3162022f6da5e1c
vauxhall <zero.griffin@gmail.com>
2016-12-21 12:11:32 -0600

    frames item 1

---
+++ looper/Model/Home/Frames/MHomeFramesItem1.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+class MHomeFramesItem1:MHomeFramesItem
+{
+    private let kFramesPerSecond:TimeInterval = 1
+    
+    override init()
+    {
+        let timeInterval:TimeInterval = 1 / kFramesPerSecond
+        let name:String = "\(timeInterval)"
+        
+        super.init(
+            timeInterval:timeInterval,
+            name:name)
+    }
+    
+    override init(timeInterval:TimeInterval, name:String)
+    {
+        fatalError()
+    }
+}
