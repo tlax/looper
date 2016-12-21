e89feb7e43b0f95f3ff0e7b4bfa0eb392c8c79d9
vauxhall <zero.griffin@gmail.com>
2016-12-21 12:08:42 -0600

    frames item abstract init

--- looper/Model/Home/Frames/MHomeFrames.swift
+++ looper/Model/Home/Frames/MHomeFrames.swift
@@ -7,5 +7,8 @@ class MHomeFrames
     init()
     {
         
+        
+        items = [
+        ]
     }
 }
--- looper/Model/Home/Frames/MHomeFramesItem.swift
+++ looper/Model/Home/Frames/MHomeFramesItem.swift
@@ -2,5 +2,17 @@ import Foundation
 
 class MHomeFramesItem
 {
+    let timeInterval:TimeInterval
+    let name:String
     
+    init(timeInterval:TimeInterval, name:String)
+    {
+        self.timeInterval = timeInterval
+        self.name = name
+    }
+    
+    init()
+    {
+        fatalError()
+    }
 }
