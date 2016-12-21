91daa4194b27075b82633f888391c3d41b2d023e
vauxhall <zero.griffin@gmail.com>
2016-12-21 10:46:42 -0600

    button add frames and constraints

--- looper/View/Home/Control/Camera/VHomeControlCameraTickerFrames.swift
+++ looper/View/Home/Control/Camera/VHomeControlCameraTickerFrames.swift
@@ -3,6 +3,13 @@ import UIKit
 class VHomeControlCameraTickerFrames:UIView
 {
     private weak var controller:CHome!
+    private weak var buttonAdd:UIButton!
+    private weak var buttonRest:UIButton!
+    private weak var label:UILabel!
+    private weak var layoutAddTop:NSLayoutConstraint!
+    private weak var layoutRestBottom:NSLayoutConstraint!
+    private let kButtonsHeight:CGFloat = 50
+    private let kButtonsWidth:CGFloat = 60
     
     convenience init(controller:CHome)
     {
@@ -10,5 +17,55 @@ class VHomeControlCameraTickerFrames:UIView
         clipsToBounds = true
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
+        
+        let buttonAdd:UIButton = UIButton()
+        buttonAdd.translatesAutoresizingMaskIntoConstraints = false
+        buttonAdd.setImage(
+            #imageLiteral(resourceName: "assetHomeCameraFramesAdd").withRenderingMode(
+                UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.normal)
+        buttonAdd.setImage(
+            #imageLiteral(resourceName: "assetHomeCameraFramesAdd").withRenderingMode(
+                UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.highlighted)
+        buttonAdd.imageView!.contentMode = UIViewContentMode.center
+        buttonAdd.imageView!.clipsToBounds = true
+        buttonAdd.imageView!.tintColor = UIColor(white:1, alpha:0.2)
+        
+        let buttonRest:UIButton = UIButton()
+        buttonRest.translatesAutoresizingMaskIntoConstraints = false
+        buttonRest.setImage(
+            #imageLiteral(resourceName: "assetHomeCameraFramesRest").withRenderingMode(
+                UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.normal)
+        buttonRest.setImage(
+            #imageLiteral(resourceName: "assetHomeCameraFramesRest").withRenderingMode(
+                UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.highlighted)
+        buttonRest.imageView!.contentMode = UIViewContentMode.center
+        buttonRest.imageView!.clipsToBounds = true
+        buttonRest.imageView!.tintColor = UIColor(white:1, alpha:0.2)
+        
+        addSubview(buttonAdd)
+        addSubview(buttonRest)
+        
+        layoutAddTop = NSLayoutConstraint.topToTop(
+            view:buttonAdd,
+            toView:self)
+        let layoutAddHeight:NSLayoutConstraint = NSLayoutConstraint.height(
+            view:buttonAdd,
+            constant:kButtonsHeight)
+        let layoutAddRight:NSLayoutConstraint = NSLayoutConstraint.rightToRight(
+            view:buttonAdd,
+            toView:self)
+        let layoutAddWidth:NSLayoutConstraint = NSLayoutConstraint.width(
+            view:buttonAdd,
+            constant:kButtonsWidth)
+        
+        addConstraints([
+            layoutAddTop,
+            layoutAddHeight,
+            layoutAddRight,
+            layoutAddWidth])
     }
 }
