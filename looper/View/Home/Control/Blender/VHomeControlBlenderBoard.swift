11a63359310a0f646da9d1ee058054aac10f3cf4
vauxhall <zero.griffin@gmail.com>
2016-12-22 12:44:34 -0600

    viewmain constraints

--- looper/View/Home/Control/Blender/VHomeControlBlenderBoard.swift
+++ looper/View/Home/Control/Blender/VHomeControlBlenderBoard.swift
@@ -2,7 +2,10 @@ import UIKit
 
 class VHomeControlBlenderBoard:UIView
 {
+    private weak var viewMain:VHomeControlBlenderBoardMain!
     private let kMarginTop:CGFloat = 100
+    private let kMarginLeft:CGFloat = 10
+    private let kMainSize:CGFloat = 110
     
     init()
     {
@@ -11,12 +14,36 @@ class VHomeControlBlenderBoard:UIView
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
         isUserInteractionEnabled = false
+        
+        let viewMain:VHomeControlBlenderBoardMain = VHomeControlBlenderBoardMain()
+        self.viewMain = viewMain
+        
+        addSubview(viewMain)
+        
+        viewMain.layoutTop = NSLayoutConstraint.topToTop(
+            view:viewMain,
+            toView:self,
+            constant:kMarginTop)
+        viewMain.layoutLeft = NSLayoutConstraint.leftToLeft(
+            view:viewMain,
+            toView:self,
+            constant:kMarginLeft)
+        viewMain.layoutWidth = NSLayoutConstraint.width(
+            view:viewMain,
+            constant:kMainSize)
+        viewMain.layoutHeight = NSLayoutConstraint.height(
+            view:viewMain,
+            constant:kMainSize)
+        
+        addConstraints([
+            viewMain.layoutTop,
+            viewMain.layoutLeft,
+            viewMain.layoutWidth,
+            viewMain.layoutHeight])
     }
     
     required init?(coder:NSCoder)
     {
         fatalError()
     }
-    
-    
 }
--- looper/View/Home/Control/Blender/VHomeControlBlenderBoardMain.swift
+++ looper/View/Home/Control/Blender/VHomeControlBlenderBoardMain.swift
@@ -2,5 +2,26 @@ import UIKit
 
 class VHomeControlBlenderBoardMain:UIView
 {
+    weak var layoutTop:NSLayoutConstraint!
+    weak var layoutLeft:NSLayoutConstraint!
+    weak var layoutWidth:NSLayoutConstraint!
+    weak var layoutHeight:NSLayoutConstraint!
+    private let kBorderWidth:CGFloat = 2
+    private let kCornerRadius:CGFloat = 10
     
+    init()
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor(white:1, alpha:0.1)
+        translatesAutoresizingMaskIntoConstraints = false
+        isUserInteractionEnabled = false
+        layer.borderColor = UIColor.white.cgColor
+        layer.cornerRadius = kCornerRadius
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
