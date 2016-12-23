35e6ab99ecd6efa936e5845f6ddddeff839584ef
vauxhall <zero.griffin@gmail.com>
2016-12-23 10:41:29 -0600

    home image sequence raw point

---
+++ looper/Model/Home/Image/MHomeImageSequenceRawPoint.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class MHomeImageSequenceRawPoint
+{
+    let percentRadius:CGFloat
+    let percentPosX:CGFloat
+    let percentPosY:CGFloat
+    
+    init(
+        percentRadius:CGFloat,
+        percentPosX:CGFloat,
+        percentPosY:CGFloat)
+    {
+        self.percentRadius = percentRadius
+        self.percentPosX = percentPosX
+        self.percentPosY = percentPosY
+    }
+}
