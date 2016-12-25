5e39f4225c00a77dd128ffead2955cc3ff361ee7
vauxhall <zero.griffin@gmail.com>
2016-12-25 14:30:24 -0600

    menu constraints

--- looper/View/Home/Control/Sequences/VHomeControlSequences.swift
+++ looper/View/Home/Control/Sequences/VHomeControlSequences.swift
@@ -2,7 +2,9 @@ import UIKit
 
 class VHomeControlSequences:UIView
 {
+    weak var viewMenu:VHomeControlSequencesMenu!
     private weak var controller:CHome!
+    private let kMenuHeight:CGFloat = 50
     
     convenience init(controller:CHome)
     {
@@ -11,5 +13,30 @@ class VHomeControlSequences:UIView
         backgroundColor = UIColor.black
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let viewMenu:VHomeControlSequencesMenu = VHomeControlSequencesMenu(
+            controller:controller)
+        self.viewMenu = viewMenu
+        
+        addSubview(viewMenu)
+        
+        let layoutMenuHeight:NSLayoutConstraint = NSLayoutConstraint.height(
+            view:viewMenu,
+            constant:kMenuHeight)
+        let layoutMenuBottom:NSLayoutConstraint = NSLayoutConstraint.bottomToBottom(
+            view:viewMenu,
+            toView:self)
+        let layoutMenuLeft:NSLayoutConstraint = NSLayoutConstraint.leftToLeft(
+            view:viewMenu,
+            toView:self)
+        let layoutMenuRight:NSLayoutConstraint = NSLayoutConstraint.rightToRight(
+            view:viewMenu,
+            toView:self)
+        
+        addConstraints([
+            layoutMenuHeight,
+            layoutMenuBottom,
+            layoutMenuLeft,
+            layoutMenuRight])
     }
 }
