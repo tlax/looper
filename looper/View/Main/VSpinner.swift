3bd8473b4ccaff3df2f920ab3c930be92d6cc933
vauxhall <zero.griffin@gmail.com>
2016-12-26 10:35:32 -0600

    spinner assets

--- looper/View/Main/VSpinner.swift
+++ looper/View/Main/VSpinner.swift
@@ -9,7 +9,14 @@ class VSpinner:UIImageView
         super.init(frame:CGRect.zero)
         
         let images:[UIImage] = [
-            #imageLiteral(resourceName: "assetSpinner0")
+            #imageLiteral(resourceName: "assetSpinner0"),
+            #imageLiteral(resourceName: "assetSpinner1"),
+            #imageLiteral(resourceName: "assetSpinner2"),
+            #imageLiteral(resourceName: "assetSpinner3"),
+            #imageLiteral(resourceName: "assetSpinner4"),
+            #imageLiteral(resourceName: "assetSpinner5"),
+            #imageLiteral(resourceName: "assetSpinner6"),
+            #imageLiteral(resourceName: "assetSpinner7")
         ]
         
         isUserInteractionEnabled = false
