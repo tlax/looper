c2833bce1e4617c48b942058dcf521b68382a221
vauxhall <zero.griffin@gmail.com>
2016-12-27 11:56:19 -0600

    view camera background

--- looper/View/Camera/VCamera.swift
+++ looper/View/Camera/VCamera.swift
@@ -5,7 +5,6 @@ class VCamera:VView
     override init(controller:CController)
     {
         super.init(controller:controller)
-        backgroundColor = UIColor.genericDark
     }
     
     required init?(coder:NSCoder)
