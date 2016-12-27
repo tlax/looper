e3fc52a06333b8de781c9b4573f5bdf0664a18dc
vauxhall <zero.griffin@gmail.com>
2016-12-27 09:45:27 -0600

    view loops

--- looper/Controller/Loops/CLoops.swift
+++ looper/Controller/Loops/CLoops.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CLoops:CController
 {
+    private weak var viewLoops:VLoops!
     
+    override func loadView()
+    {
+        let viewLoops:VLoops = VLoops(controller:self)
+        self.viewLoops = viewLoops
+        view = viewLoops
+    }
 }
