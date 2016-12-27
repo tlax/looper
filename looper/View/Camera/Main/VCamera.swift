f325612dbb2599886f6a6ae083c4bc734b8d81e0
vauxhall <zero.griffin@gmail.com>
2016-12-27 12:34:13 -0600

    collection view in view camera

--- looper/View/Camera/Main/VCamera.swift
+++ looper/View/Camera/Main/VCamera.swift
@@ -1,8 +1,8 @@
 import UIKit
 
-class VCamera:VView
+class VCamera:VView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
-    private weak var collectionView:UICollectionView!
+    private weak var collectionView:VCollection!
     private let kHeaderHeight:CGFloat = 150
     private let kCollectionBottom:CGFloat = 50
     private let kInterLine:CGFloat = 1
@@ -11,20 +11,18 @@ class VCamera:VView
     {
         super.init(controller:controller)
         
-        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        flow.headerReferenceSize = CGSize(width:0, height:kHeaderHeight)
-        flow.footerReferenceSize = CGSize.zero
-        flow.minimumLineSpacing = kInterLine
-        flow.scrollDirection = UICollectionViewScrollDirection.vertical
-        flow.sectionInset = UIEdgeInsets(
+        let collectionView:VCollection = VCollection()
+        collectionView.flow.headerReferenceSize = CGSize(width:0, height:kHeaderHeight)
+        collectionView.flow.minimumLineSpacing = kInterLine
+        collectionView.flow.sectionInset = UIEdgeInsets(
             top:0,
             left:0,
             bottom:kCollectionBottom,
             right:0)
-        
-        let collectionView:UICollectionView = UICollectionView(
-            frame:CGRect.zero,
-            collectionViewLayout:flow)
+        collectionView.alwaysBounceVertical = true
+        collectionView.registerHeader(header:VCameraHeader.self)
+        collectionView.delegate = self
+        collectionView.dataSource = self
         self.collectionView = collectionView
         
         addSubview(collectionView)
@@ -35,6 +33,18 @@ class VCamera:VView
         let layoutCollectionBottom:NSLayoutConstraint = NSLayoutConstraint.bottomToBottom(
             view:collectionView,
             toView:self)
+        let layoutCollectionLeft:NSLayoutConstraint = NSLayoutConstraint.leftToLeft(
+            view:collectionView,
+            toView:self)
+        let layoutCollectionRight:NSLayoutConstraint = NSLayoutConstraint.rightToRight(
+            view:collectionView,
+            toView:self)
+        
+        addConstraints([
+            layoutCollectionTop,
+            layoutCollectionBottom,
+            layoutCollectionLeft,
+            layoutCollectionRight])
     }
     
     required init?(coder:NSCoder)
--- looper/View/Main/Subclasses/VCollection.swift
+++ looper/View/Main/Subclasses/VCollection.swift
@@ -7,6 +7,13 @@ class VCollection:UICollectionView
     init()
     {
         let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize.zero
+        flow.minimumLineSpacing = 0
+        flow.minimumInteritemSpacing = 0
+        flow.itemSize = CGSize.zero
+        flow.sectionInset = UIEdgeInsets.zero
+        flow.scrollDirection = UICollectionViewScrollDirection.vertical
         self.flow = flow
         
         super.init(frame:CGRect.zero, collectionViewLayout:flow)
@@ -31,11 +38,19 @@ class VCollection:UICollectionView
             forCellWithReuseIdentifier:cell.reusableIdentifier)
     }
     
-    func registerReusable(reusable:UICollectionReusableView.Type, kind:String)
+    func registerFooter(footer:UICollectionReusableView.Type)
     {
         register(
-            reusable,
-            forSupplementaryViewOfKind:kind,
-            withReuseIdentifier:reusable.reusableIdentifier)
+            footer,
+            forSupplementaryViewOfKind:UICollectionElementKindSectionFooter,
+            withReuseIdentifier:footer.reusableIdentifier)
+    }
+    
+    func registerHeader(header:UICollectionReusableView.Type)
+    {
+        register(
+            header,
+            forSupplementaryViewOfKind:UICollectionElementKindSectionHeader,
+            withReuseIdentifier:header.reusableIdentifier)
     }
 }
