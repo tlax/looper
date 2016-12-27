b3ff0c7e869917ec60a0defe7b3ec2b3096bb350
vauxhall <zero.griffin@gmail.com>
2016-12-27 10:43:03 -0600

    button loops

--- looper/View/Main/VParentBar.swift
+++ looper/View/Main/VParentBar.swift
@@ -3,7 +3,11 @@ import UIKit
 class VParentBar:UIView
 {
     private weak var controller:CParent!
-    private weak var buttonLoops:UIButton!
+    private weak var buttonLoops:VParentBarButton!
+    private weak var buttonCamera:VParentBarButton!
+    private weak var buttonStore:VParentBarButton!
+    private weak var layoutLoopsLeft:NSLayoutConstraint!
+    private let kButtonsWidth:CGFloat = 60
     
     convenience init(controller:CParent)
     {
@@ -13,13 +17,38 @@ class VParentBar:UIView
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
         
-        let buttonLoops:UIButton = UIButton()
-        buttonLoops.translatesAutoresizingMaskIntoConstraints = false
-        buttonLoops.imageView!.contentMode = UIViewContentMode.center
-        buttonLoops.imageView!.clipsToBounds = true
-        
+        let buttonLoops:VParentBarButton = VParentBarButton(image:#imageLiteral(resourceName: "assetHomeCamera"))
         self.buttonLoops = buttonLoops
         
         addSubview(buttonLoops)
+        
+        let layoutLoopsTop:NSLayoutConstraint = NSLayoutConstraint.topToTop(
+            view:buttonLoops,
+            toView:self)
+        let layoutLoopsBottom:NSLayoutConstraint = NSLayoutConstraint.bottomToBottom(
+            view:buttonLoops,
+            toView:self)
+        layoutLoopsLeft = NSLayoutConstraint.leftToLeft(
+            view:buttonLoops,
+            toView:self)
+        let layoutLoopsWidth:NSLayoutConstraint = NSLayoutConstraint.width(
+            view:buttonLoops,
+            constant:kButtonsWidth)
+        
+        addConstraints([
+            layoutLoopsTop,
+            layoutLoopsBottom,
+            layoutLoopsLeft,
+            layoutLoopsWidth])
+    }
+    
+    override func layoutSubviews()
+    {
+        let width:CGFloat = bounds.maxX
+        let remainLoops:CGFloat = width - kButtonsWidth
+        let marginLoops:CGFloat = remainLoops / 2.0
+        layoutLoopsLeft.constant = marginLoops
+        
+        super.layoutSubviews()
     }
 }
