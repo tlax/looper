61e7279e6a2e854bd44ba1ec827f6db7b0dacd96
vauxhall <zero.griffin@gmail.com>
2016-12-28 16:22:06 -0600

    camera loading and load finished notification

--- looper/Extensions/ExtensionNotification.swift
+++ looper/Extensions/ExtensionNotification.swift
@@ -3,4 +3,6 @@ import Foundation
 extension Notification
 {
     static let imagesUpdated:Notification.Name = Notification.Name("imagesUpdated")
+    static let cameraLoading:Notification.Name = Notification.Name("cameraLoading")
+    static let cameraLoadFinished:Notification.Name = Notification.Name("cameraLoadFinished")
 }
