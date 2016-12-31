2ba4138462478dc9c73cc4fb549203ad78feab4b
vauxhall <zero.griffin@gmail.com>
2016-12-31 17:45:56 -0600

    model camera filter item

--- looper/Model/Camera/Filter/MCameraFilterItem.swift
+++ looper/Model/Camera/Filter/MCameraFilterItem.swift
@@ -1,9 +1,6 @@
-//
-//  MCameraFilterItem.swift
-//  looper
-//
-//  Created by zero on 12/31/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MCameraFilterItem
+{
+    
+}
