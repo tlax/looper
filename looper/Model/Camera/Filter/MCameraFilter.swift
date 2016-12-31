a97e2105e10aecc617e303878158964e4c53f663
vauxhall <zero.griffin@gmail.com>
2016-12-31 17:46:58 -0600

    model camera filter init

--- looper/Model/Camera/Filter/MCameraFilter.swift
+++ looper/Model/Camera/Filter/MCameraFilter.swift
@@ -2,5 +2,14 @@ import Foundation
 
 class MCameraFilter
 {
+    weak var currentFilter:MCameraFilterItem?
+    let items:[MCameraFilterItem]
     
+    init()
+    {
+        items = [
+        ]
+        
+        currentFilter = items.first
+    }
 }
