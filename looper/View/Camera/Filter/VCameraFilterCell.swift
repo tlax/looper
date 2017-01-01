af2466497fa2184134e67294a961c72e475e64eb
vauxhall <zero.griffin@gmail.com>
2016-12-31 18:38:06 -0600

    filter cell hover

--- looper/View/Camera/Filter/VCameraFilterCell.swift
+++ looper/View/Camera/Filter/VCameraFilterCell.swift
@@ -2,5 +2,63 @@ import UIKit
 
 class VCameraFilterCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
+    private weak var label:UILabel!
+    private weak var selectedIcon:UIImageView!
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        self.imageView = imageView
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            selectedIcon.isHidden = false
+        }
+        else
+        {
+            selectedIcon.isHidden = true
+        }
+    }
+    
+    //MARK: public
+    
+    func config(model:MCameraFilterItem)
+    {
+        imageView.image = model.image
+        label.text = model.title
+        
+        hover()
+    }
 }
