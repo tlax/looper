a79f314a94d5a638cdac7c1d0b902e1097fdfb20
vauxhall <zero.griffin@gmail.com>
2017-01-01 21:48:18 -0600

    init model camera compress

--- looper/Model/Camera/Compress/MCameraCompress.swift
+++ looper/Model/Camera/Compress/MCameraCompress.swift
@@ -2,5 +2,23 @@ import Foundation
 
 class MCameraCompress
 {
+    let items:[MCameraCompressItem]
+    var currentCompress:MCameraCompressItem?
     
+    init()
+    {
+        let itemNone:MCameraCompressItemNone = MCameraCompressItemNone()
+        let itemSlight:MCameraCompressItemSlight = MCameraCompressItemSlight()
+        let itemMedium:MCameraCompressItemMedium = MCameraCompressItemMedium()
+        let itemMax:MCameraCompressItemMax = MCameraCompressItemMax()
+        
+        items = [
+            itemNone,
+            itemSlight,
+            itemMedium,
+            itemMax
+        ]
+        
+        currentCompress = items.first
+    }
 }
