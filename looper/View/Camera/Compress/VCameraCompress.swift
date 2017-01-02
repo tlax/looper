51934ded9066113c9d8d217a966304841a68a696
vauxhall <zero.griffin@gmail.com>
2017-01-01 21:41:02 -0600

    init view camera compress

--- looper/View/Camera/Compress/VCameraCompress.swift
+++ looper/View/Camera/Compress/VCameraCompress.swift
@@ -2,5 +2,16 @@ import UIKit
 
 class VCameraCompress:VView
 {
+    private weak var controller:CCameraCompress!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        self.controller = controller as? CCameraCompress
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
