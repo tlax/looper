0703b0f7684671651c6e319ea5f37094bccd522b
vauxhall <zero.griffin@gmail.com>
2017-01-01 21:39:46 -0600

    controller compress uses view compress

--- looper/Controller/Camera/CCameraCompress.swift
+++ looper/Controller/Camera/CCameraCompress.swift
@@ -2,5 +2,36 @@ import UIKit
 
 class CCameraCompress:CController
 {
+    weak var model:MCamera!
+    private weak var viewCompress:VCameraCompress!
     
+    init(model:MCamera)
+    {
+        self.model = model
+        super.init()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    override func loadView()
+    {
+        let viewCompress:VCameraCompress = VCameraCompress(controller:self)
+        self.viewCompress = viewCompress
+        view = viewCompress
+    }
+    
+    //MARK: public
+    
+    func back()
+    {
+        parentController.pop(horizontal:CParent.TransitionHorizontal.fromRight)
+    }
+    
+    func next()
+    {
+        
+    }
 }
