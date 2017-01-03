b785699bb1cea1e15a13232fec5bcb7f21a2bea9
vauxhall <zero.griffin@gmail.com>
2017-01-03 17:24:38 -0600

    parsing model loops

--- looper/Model/Loops/MLoops.swift
+++ looper/Model/Loops/MLoops.swift
@@ -2,5 +2,59 @@ import Foundation
 
 class MLoops
 {
+    var items:[MLoopsItem]
     
+    init()
+    {
+        items = []
+    }
+    
+    //MARK: private
+    
+    private func asyncLoadFromDb()
+    {
+        DManager.sharedInstance.fetchManagedObjects(
+            entityName:DLoop.entityName)
+        { [weak self] (fetched) in
+            
+            guard
+                
+                let loops:[DLoop] = fetched as? [DLoop]
+                
+            else
+            {
+                self?.items = []
+                self?.finishedLoading()
+                
+                return
+            }
+            
+            var items:[MLoopsItem] = []
+            
+            for loop:DLoop in loops
+            {
+                let item:MLoopsItem = MLoopsItem(loop:loop)
+                items.append(item)
+            }
+            
+            self?.items = items
+            self?.finishedLoading()
+        }
+    }
+    
+    private func finishedLoading()
+    {
+        
+    }
+    
+    //MARK: public
+    
+    func loadFromDb()
+    {
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            self?.asyncLoadFromDb()
+        }
+    }
 }
--- looper/Model/Loops/MLoopsItem.swift
+++ looper/Model/Loops/MLoopsItem.swift
@@ -2,5 +2,22 @@ import UIKit
 
 class MLoopsItem
 {
+    private(set) var images:[UIImage]
     
+    init(loop:DLoop)
+    {
+        images = []
+        
+        guard
+            
+            let folderName:String = loop.folder
+        
+        else
+        {
+            return
+        }
+        
+        let appDirectory:URL = FileManager.appDirectory
+        let folderPath:URL = appDirectory.appendingPathComponent(folderName)
+    }
 }
