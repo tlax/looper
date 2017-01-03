9c270f1062d19244bf7ba5af01160f91aa51e9ef
vauxhall <zero.griffin@gmail.com>
2017-01-03 17:25:22 -0600

    controller loops init

--- looper/Controller/Loops/CLoops.swift
+++ looper/Controller/Loops/CLoops.swift
@@ -3,8 +3,19 @@ import UIKit
 class CLoops:CController
 {
     weak var viewLoops:VLoops!
-    let model:mloo
+    let model:MLoops
 
+    override init()
+    {
+        model = MLoops()
+        super.init()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
     override func loadView()
     {
         let viewLoops:VLoops = VLoops(controller:self)
