9bc5aebd8f079edfc6338be0b1c77ce738f7ff03
vauxhall <zero.griffin@gmail.com>
2017-01-03 09:04:27 -0600

    hiding bar on preview controller

--- looper/Controller/Camera/CCameraPreview.swift
+++ looper/Controller/Camera/CCameraPreview.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class CCameraPreview:CController
 {
+    private weak var viewPreview:VCameraPreview!
     let model:MCameraRecord
     
     init(model:MCameraRecord)
@@ -10,8 +11,27 @@ class CCameraPreview:CController
         super.init()
     }
     
+    override func viewDidAppear(_ animated:Bool)
+    {
+        super.viewDidAppear(animated)
+        parentController.changeBar(barHidden:true)
+    }
+    
+    override func viewWillDisappear(_ animated:Bool)
+    {
+        super.viewWillDisappear(animated)
+        parentController.changeBar(barHidden:false)
+    }
+    
     required init?(coder:NSCoder)
     {
         fatalError()
+    }
+    
+    override func loadView()
+    {
+        let viewPreview:VCameraPreview = VCameraPreview(controller:self)
+        self.viewPreview = viewPreview
+        view = viewPreview
     }
 }
