10788e5d29d0ecf23abfbc7c6b1f70f7bff7ce42
vauxhall <zero.griffin@gmail.com>
2017-01-03 09:21:17 -0600

    view camera preview display

---
+++ looper/View/Camera/Preview/VCameraPreviewDisplay.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+class VCameraPreviewDisplay:UIView
+{
+    weak var imageView:UIImageView!
+    
+    init()
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        let layoutImageTop:NSLayoutConstraint = NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self)
+        let layoutImageBottom:NSLayoutConstraint = NSLayoutConstraint.bottomToBottom(
+            view:imageView,
+            toView:self)
+        let layoutImageLeft:NSLayoutConstraint = NSLayoutConstraint.leftToLeft(
+            view:imageView,
+            toView:self)
+        let layoutImageRight:NSLayoutConstraint = NSLayoutConstraint.rightToRight(
+            view:imageView,
+            toView:self)
+        
+        addConstraints([
+            layoutImageTop,
+            layoutImageBottom,
+            layoutImageLeft,
+            layoutImageRight])
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+}
