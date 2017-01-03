0e232e2c1334df8181b48864b638baf61eb34a87
vauxhall <zero.griffin@gmail.com>
2017-01-02 19:10:12 -0600

    label percent

--- looper/View/Camera/Compress/VCameraCompressCell.swift
+++ looper/View/Camera/Compress/VCameraCompressCell.swift
@@ -4,9 +4,11 @@ class VCameraCompressCell:UICollectionViewCell
 {
     private weak var imageView:UIImageView!
     private weak var label:UILabel!
+    private weak var percent:UILabel!
     private weak var selectedIcon:UIImageView!
     private let kLabelLeft:CGFloat = 10
-    private let kSelectedWidth:CGFloat = 55
+    private let kLabelWidth:CGFloat = 70
+    private let kSelectedWidth:CGFloat = 75
     private let kAlphaSelected:CGFloat = 1
     private let kAlphaNotSelected:CGFloat = 0.4
     
@@ -25,6 +27,15 @@ class VCameraCompressCell:UICollectionViewCell
         label.numberOfLines = 0
         self.label = label
         
+        let percent:UILabel = UILabel()
+        percent.isUserInteractionEnabled = false
+        percent.translatesAutoresizingMaskIntoConstraints = false
+        percent.backgroundColor = UIColor.clear
+        percent.font = UIFont.bold(size:25)
+        percent.textColor = UIColor.genericLight
+        percent.textAlignment = NSTextAlignment.right
+        self.percent = percent
+        
         let selectedIcon:UIImageView = UIImageView()
         selectedIcon.translatesAutoresizingMaskIntoConstraints = false
         selectedIcon.clipsToBounds = true
@@ -34,6 +45,7 @@ class VCameraCompressCell:UICollectionViewCell
         self.selectedIcon = selectedIcon
         
         addSubview(label)
+        addSubview(percent)
         addSubview(selectedIcon)
         
         let layoutSelectedTop:NSLayoutConstraint = NSLayoutConstraint.topToTop(
@@ -63,6 +75,19 @@ class VCameraCompressCell:UICollectionViewCell
             view:label,
             toView:selectedIcon)
         
+        let layoutPercentTop:NSLayoutConstraint = NSLayoutConstraint.topToTop(
+            view:percent,
+            toView:self)
+        let layoutPercentBottom:NSLayoutConstraint = NSLayoutConstraint.bottomToBottom(
+            view:percent,
+            toView:self)
+        let layoutPercentLeft:NSLayoutConstraint = NSLayoutConstraint.leftToRight(
+            view:percent,
+            toView:label)
+        let layoutPercentRight:NSLayoutConstraint = NSLayoutConstraint.rightToLeft(
+            view:percent,
+            toView:selectedIcon)
+        
         addConstraints([
             layoutSelectedTop,
             layoutSelectedBottom,
@@ -71,7 +96,11 @@ class VCameraCompressCell:UICollectionViewCell
             layoutLabelTop,
             layoutLabelBottom,
             layoutLabelLeft,
-            layoutLabelRight])
+            layoutLabelRight,
+            layoutPercentTop,
+            layoutPercentBottom,
+            layoutPercentLeft,
+            layoutPercentRight])
     }
     
     required init?(coder:NSCoder)
@@ -103,11 +132,13 @@ class VCameraCompressCell:UICollectionViewCell
         {
             selectedIcon.isHidden = false
             label.alpha = kAlphaSelected
+            percent.alpha = kAlphaSelected
         }
         else
         {
             selectedIcon.isHidden = true
             label.alpha = kAlphaNotSelected
+            percent.alpha = kAlphaNotSelected
         }
     }
     
