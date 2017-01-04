2005bb938370061089ce31ce661d4437054eda9a
vauxhall <zero.griffin@gmail.com>
2017-01-04 14:27:24 -0600

    notiication loops stop

--- looper/Extensions/ExtensionNotification.swift
+++ looper/Extensions/ExtensionNotification.swift
@@ -6,4 +6,5 @@ extension Notification
     static let cameraLoading:Notification.Name = Notification.Name("cameraLoading")
     static let cameraLoadFinished:Notification.Name = Notification.Name("cameraLoadFinished")
     static let cameraControlsScroll:Notification.Name = Notification.Name("cameraControlsScroll")
+    static let loopsPause:Notification.Name = Notification.Name("loopsPause")
 }
