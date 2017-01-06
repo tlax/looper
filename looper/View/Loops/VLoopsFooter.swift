a3b7a10a199ca39115d1c47d2f4f88b32bb809d8
vauxhall <zero.griffin@gmail.com>
2017-01-06 17:41:07 -0600

    asset loops help

--- looper/View/Loops/VLoopsFooter.swift
+++ looper/View/Loops/VLoopsFooter.swift
@@ -5,6 +5,10 @@ class VLoopsFooter:UICollectionReusableView
     override init(frame:CGRect)
     {
         super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        
+        
     }
     
     required init?(coder:NSCoder)
