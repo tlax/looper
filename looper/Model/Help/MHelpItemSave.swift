495b116eef66912c4ddbb44b3e8cc4f8518b1b48
vauxhall <zero.griffin@gmail.com>
2017-01-07 17:07:21 -0600

    model item save

---
+++ looper/Model/Help/MHelpItemSave.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class MHelpItemSave:MHelpItem
+{
+    override init()
+    {
+        let image:UIImage = #imageLiteral(resourceName: "assetHelpFilter")
+        let title:String = NSLocalizedString("MHelpItemSave_title", comment:"")
+        
+        super.init(
+            image:image,
+            title:title)
+    }
+    
+    override init(image:UIImage, title:String)
+    {
+        fatalError()
+    }
+}
