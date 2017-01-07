58ff9a64a615c34887e48eaeb5206d159fad0596
vauxhall <zero.griffin@gmail.com>
2017-01-06 18:21:05 -0600

    blur in view help

--- looper/View/Help/VHelp.swift
+++ looper/View/Help/VHelp.swift
@@ -10,6 +10,20 @@ class VHelp:VView
         backgroundColor = UIColor.clear
         self.controller = controller as? CHelp
         
+        let blurEffect:UIBlurEffect = UIBlurEffect(style:UIBlurEffectStyle.extraLight)
+        let visualEffect:UIVisualEffectView = UIVisualEffectView(
+            effect:blurEffect)
+        visualEffect.isUserInteractionEnabled = false
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        visualEffect.clipsToBounds = true
+        
+        addSubview(visualEffect)
+        
+        let constraintsEffect:[NSLayoutConstraint] = NSLayoutConstraint.equals(
+            view:visualEffect,
+            parent:self)
+        
+        addConstraints(constraintsEffect)
     }
     
     required init?(coder:NSCoder)
