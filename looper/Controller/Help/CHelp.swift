c508f648ef7689fcec5fda5a0e2685fc23132f45
vauxhall <zero.griffin@gmail.com>
2017-01-06 18:32:39 -0600

    controller help push/pop

--- looper/Controller/Help/CHelp.swift
+++ looper/Controller/Help/CHelp.swift
@@ -2,5 +2,11 @@ import UIKit
 
 class CHelp:CController
 {
+    //MARK: public
     
+    func back()
+    {
+        parentController.pop(
+            vertical:CParent.TransitionVertical.fromBottom)
+    }
 }
--- looper/View/Help/VHelp.swift
+++ looper/View/Help/VHelp.swift
@@ -3,6 +3,11 @@ import UIKit
 class VHelp:VView
 {
     private weak var controller:CHelp!
+    private weak var layoutButtonLeft:NSLayoutConstraint!
+    private let kButtonWidth:CGFloat = 120
+    private let kButtonHeight:CGFloat = 32
+    private let kButtonBottom:CGFloat = -30
+    private let kCornerRadius:CGFloat = 15
     
     override init(controller:CController)
     {
@@ -17,17 +22,75 @@ class VHelp:VView
         visualEffect.translatesAutoresizingMaskIntoConstraints = false
         visualEffect.clipsToBounds = true
         
+        let button:UIButton = UIButton()
+        button.backgroundColor = UIColor.genericLight
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.setTitle(
+            NSLocalizedString("VHelp_button", comment:""),
+            for:UIControlState.normal)
+        button.setTitleColor(
+            UIColor.white,
+            for:UIControlState.normal)
+        button.setTitleColor(
+            UIColor(white:1, alpha:0.2),
+            for:UIControlState.highlighted)
+        button.titleLabel!.font = UIFont.regular(size:15)
+        button.layer.cornerRadius = kCornerRadius
+        button.addTarget(
+            self,
+            action:#selector(actionButton(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
         addSubview(visualEffect)
+        addSubview(button)
         
         let constraintsEffect:[NSLayoutConstraint] = NSLayoutConstraint.equals(
             view:visualEffect,
             parent:self)
         
+        let layoutButtonBottom:NSLayoutConstraint = NSLayoutConstraint.bottomToBottom(
+            view:button,
+            toView:self,
+            constant:kButtonBottom)
+        let layoutButtonHeight:NSLayoutConstraint = NSLayoutConstraint.height(
+            view:button,
+            constant:kButtonHeight)
+        layoutButtonLeft = NSLayoutConstraint.leftToLeft(
+            view:button,
+            toView:self)
+        let layoutButtonWidth:NSLayoutConstraint = NSLayoutConstraint.width(
+            view:button,
+            constant:kButtonWidth)
+        
         addConstraints(constraintsEffect)
+        
+        addConstraints([
+            layoutButtonBottom,
+            layoutButtonHeight,
+            layoutButtonLeft,
+            layoutButtonWidth])
     }
     
     required init?(coder:NSCoder)
     {
         fatalError()
+    }
+    
+    override func layoutSubviews()
+    {
+        let width:CGFloat = bounds.maxX
+        let remain:CGFloat = width - kButtonWidth
+        let margin:CGFloat = remain / 2.0
+        layoutButtonLeft.constant = margin
+        
+        super.layoutSubviews()
+    }
+    
+    //MARK: actions
+    
+    func actionButton(sender button:UIButton)
+    {
+        controller.back()
     }
 }
