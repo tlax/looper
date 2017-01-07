73419d69a428d98ca4645640c753b70b55214413
vauxhall <zero.griffin@gmail.com>
2017-01-06 20:54:44 -0600

    image view in help cell

--- looper/View/Help/VHelpCell.swift
+++ looper/View/Help/VHelpCell.swift
@@ -2,12 +2,45 @@ import UIKit
 
 class VHelpCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
+    private let kImageTop:CGFloat = 20
+    private let kImageHeight:CGFloat = 180
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
         isUserInteractionEnabled = false
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        imageView.clipsToBounds = true
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        let layoutImageTop:NSLayoutConstraint = NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self,
+            constant:kImageTop)
+        let layoutImageHeight:NSLayoutConstraint = NSLayoutConstraint.height(
+            view:imageView,
+            constant:kImageHeight)
+        let layoutImageLeft:NSLayoutConstraint = NSLayoutConstraint.leftToLeft(
+            view:imageView,
+            toView:self)
+        let layoutImageRight:NSLayoutConstraint = NSLayoutConstraint.rightToRight(
+            view:imageView,
+            toView:self)
+        
+        addConstraints([
+            layoutImageTop,
+            layoutImageHeight,
+            layoutImageLeft,
+            layoutImageRight])
     }
     
     required init?(coder:NSCoder)
@@ -19,6 +52,7 @@ class VHelpCell:UICollectionViewCell
     
     func config(model:MHelpItem)
     {
+        imageView.image = model.image
         
     }
 }
