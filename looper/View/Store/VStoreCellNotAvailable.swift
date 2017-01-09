77300e60c8c70a6ffa73f0496c53ce323444cff4
vauxhall <zero.griffin@gmail.com>
2017-01-09 08:57:15 -0600

    view store cell not available

--- looper/View/Store/VStoreCellNotAvailable.swift
+++ looper/View/Store/VStoreCellNotAvailable.swift
@@ -1,20 +1,20 @@
 import UIKit
 
-class VStoreCellPurchased:VStoreCell
+class VStoreCellNotAvailable:VStoreCell
 {
     override init(frame:CGRect)
     {
         super.init(frame:frame)
-        backgroundColor = UIColor.genericLight
+        backgroundColor = UIColor.clear
         isUserInteractionEnabled = false
         
         let label:UILabel = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.isUserInteractionEnabled = false
         label.textAlignment = NSTextAlignment.center
-        label.font = UIFont.medium(size:16)
-        label.textColor = UIColor.white
-        label.text = NSLocalizedString("VStoreCellPurchased_label", comment:"")
+        label.font = UIFont.regular(size:14)
+        label.textColor = UIColor(white:0.5, alpha:1)
+        label.text = NSLocalizedString("VStoreCellNotAvailable_label", comment:"")
         
         addSubview(label)
         
