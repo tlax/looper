f44c51fef8d5ff911eb5d972d05eeb36090fce01
vauxhall <zero.griffin@gmail.com>
2017-01-09 09:16:59 -0600

    item status purchasing

--- looper/Model/Store/MStoreItemStatusPurchasing.swift
+++ looper/Model/Store/MStoreItemStatusPurchasing.swift
@@ -1,9 +1,21 @@
-//
-//  MStoreItemStatusPurchasing.swift
-//  looper
-//
-//  Created by zero on 1/9/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class MStoreItemStatusPurchasing:MStoreItemStatus
+{
+    private let kCellHeight:CGFloat = 30
+    private let kRestorable:Bool = false
+    
+    override init()
+    {
+        let reusableIdentifier:String = VStoreCellPurchasing.reusableIdentifier
+        super.init(
+            reusableIdentifier:reusableIdentifier,
+            cellHeight:kCellHeight,
+            restorable:kRestorable)
+    }
+    
+    override init(reusableIdentifier:String, cellHeight:CGFloat, restorable:Bool)
+    {
+        fatalError()
+    }
+}
