8fa1e27cc2d8c5dc18cc6cf7ec1c89efd705c282
vauxhall <zero.griffin@gmail.com>
2017-01-09 16:01:56 -0600

    process controller filter blender

--- looper/Controller/Camera/CCameraFilterBlender.swift
+++ looper/Controller/Camera/CCameraFilterBlender.swift
@@ -2,5 +2,31 @@ import UIKit
 
 class CCameraFilterBlender:CController
 {
+    private weak var model:MCameraFilterItemBlend!
+    private weak var viewBlender:VCameraFilterBlender!
     
+    init(model:MCameraFilterItemBlend)
+    {
+        self.model = model
+        super.init()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    override func loadView()
+    {
+        let viewBlender:VCameraFilterBlender = VCameraFilterBlender(controller:self)
+        self.viewBlender = viewBlender
+        view = viewBlender
+    }
+    
+    //MARK: public
+    
+    func back()
+    {
+        parentController.pop(horizontal:CParent.TransitionHorizontal.fromRight)
+    }
 }
--- looper/View/Camera/Filter/Blender/VCameraFilterBlender.swift
+++ looper/View/Camera/Filter/Blender/VCameraFilterBlender.swift
@@ -1,18 +1,122 @@
 import UIKit
 
-class VCameraFilterBlender:VView
+class VCameraFilterBlender:VView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CCameraFilterBlender!
+    private weak var collectionView:VCollection!
+    private let kContentTop:CGFloat = 20
+    private let kBackWidth:CGFloat = 55
+    private let kButtonsHeight:CGFloat = 44
     
     override init(controller:CController)
     {
         super.init(controller:controller)
-        
         self.controller = controller as? CCameraFilterBlender
+        
+        let backButton:UIButton = UIButton()
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.setImage(
+            #imageLiteral(resourceName: "assetGenericBack").withRenderingMode(UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.normal)
+        backButton.setImage(
+            #imageLiteral(resourceName: "assetGenericBack").withRenderingMode(UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.highlighted)
+        backButton.imageView!.contentMode = UIViewContentMode.center
+        backButton.imageView!.clipsToBounds = true
+        backButton.imageView!.tintColor = UIColor(white:0, alpha:0.2)
+        backButton.addTarget(
+            self,
+            action:#selector(actionBack(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
+        let nextButton:UIButton = UIButton()
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        nextButton.setImage(
+            #imageLiteral(resourceName: "assetGenericNext").withRenderingMode(
+                UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.normal)
+        nextButton.setImage(
+            #imageLiteral(resourceName: "assetGenericNext").withRenderingMode(
+                UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.highlighted)
+        nextButton.imageView!.contentMode = UIViewContentMode.center
+        nextButton.imageView!.tintColor = UIColor(white:0, alpha:0.2)
+        nextButton.imageView!.clipsToBounds = true
+        nextButton.addTarget(
+            self,
+            action:#selector(actionNext(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
+        let collectionView:VCollection = VCollection()
+        collectionView.flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+        collectionView.alwaysBounceHorizontal = true
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
+        
+        let constraintsCollection:[NSLayoutConstraint] = NSLayoutConstraint.equals(
+            view:collectionView,
+            parent:self)
+        
+        let layoutBackTop:NSLayoutConstraint = NSLayoutConstraint.topToTop(
+            view:backButton,
+            toView:self,
+            constant:kContentTop)
+        let layoutBackHeight:NSLayoutConstraint = NSLayoutConstraint.height(
+            view:backButton,
+            constant:kButtonsHeight)
+        let layoutBackLeft:NSLayoutConstraint = NSLayoutConstraint.leftToLeft(
+            view:backButton,
+            toView:self)
+        let layoutBackWidth:NSLayoutConstraint = NSLayoutConstraint.width(
+            view:backButton,
+            constant:kBackWidth)
+        
+        addConstraints(constraintsCollection)
+        
+        addConstraints([
+            layoutBackTop,
+            layoutBackHeight,
+            layoutBackLeft,
+            layoutBackWidth])
     }
     
     required init?(coder:NSCoder)
     {
         fatalError()
+    }
+    
+    //MARK: actions
+    
+    func actionBack(sender button:UIButton)
+    {
+        controller?.back()
+    }
+    
+    func actionNext(sender button:UIButton)
+    {
+        
+    }
+    
+    //MARK: collectionView delegate
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        return 0
+    }
+    
+    func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let cell:VCameraFilterBlenderCell = collectionView.dequeueReusableCell(
+            withReuseIdentifier:
+            VCameraFilterBlenderCell.reusableIdentifier,
+            for:indexPath) as! VCameraFilterBlenderCell
+        
+        return cell
     }
 }
