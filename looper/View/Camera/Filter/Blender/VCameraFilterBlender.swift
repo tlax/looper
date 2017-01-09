f98afdcc4cad50e1ebad59b99591c02ed9580f40
vauxhall <zero.griffin@gmail.com>
2017-01-09 15:37:49 -0600

    controller camera filter blender overlay

--- looper/View/Camera/Filter/Blender/VCameraFilterBlender.swift
+++ looper/View/Camera/Filter/Blender/VCameraFilterBlender.swift
@@ -2,5 +2,17 @@ import UIKit
 
 class VCameraFilterBlender:VView
 {
+    private weak var controller:CCameraFilterBlender!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        
+        self.controller = controller as? CCameraFilterBlender
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
