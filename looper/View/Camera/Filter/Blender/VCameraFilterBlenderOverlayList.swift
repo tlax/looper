3561035bdcc65df1183503def0686b6731fdea7b
vauxhall <zero.griffin@gmail.com>
2017-01-10 10:18:13 -0600

    action add

--- looper/View/Camera/Filter/Blender/VCameraFilterBlenderOverlayList.swift
+++ looper/View/Camera/Filter/Blender/VCameraFilterBlenderOverlayList.swift
@@ -4,9 +4,9 @@ class VCameraFilterBlenderOverlayList:UIView
 {
     private weak var controller:CCameraFilterBlenderOverlay!
     private weak var buttonAdd:VCameraFilterBlenderOverlayListAdd!
-    private weak var layoutAddTop:NSLayoutConstraint!
-    private weak var layoutAddLeft:NSLayoutConstraint!
+    private let kAfterInit:TimeInterval = 1
     private let kButtonAddSize:CGFloat = 80
+    private let kButtonAddTop:CGFloat = 10
     
     convenience init(controller:CCameraFilterBlenderOverlay)
     {
@@ -17,19 +17,25 @@ class VCameraFilterBlenderOverlayList:UIView
         self.controller = controller
         
         let buttonAdd:VCameraFilterBlenderOverlayListAdd = VCameraFilterBlenderOverlayListAdd()
+        buttonAdd.addTarget(
+            self,
+            action:#selector(actionAdd(sender:)),
+            for:UIControlEvents.touchUpInside)
         self.buttonAdd = buttonAdd
         
         addSubview(buttonAdd)
         
-        layoutAddTop = NSLayoutConstraint.topToTop(
+        let layoutAddTop:NSLayoutConstraint = NSLayoutConstraint.topToTop(
             view:buttonAdd,
-            toView:self)
+            toView:self,
+            constant:kButtonAddTop)
         let layoutAddHeight:NSLayoutConstraint = NSLayoutConstraint.height(
             view:buttonAdd,
             constant:kButtonAddSize)
-        layoutAddLeft = NSLayoutConstraint.leftToLeft(
+        buttonAdd.layoutLeft = NSLayoutConstraint.leftToLeft(
             view:buttonAdd,
-            toView:self)
+            toView:self,
+            constant:-kButtonAddSize)
         let layoutAddWidth:NSLayoutConstraint = NSLayoutConstraint.width(
             view:buttonAdd,
             constant:kButtonAddSize)
@@ -37,21 +43,21 @@ class VCameraFilterBlenderOverlayList:UIView
         addConstraints([
             layoutAddTop,
             layoutAddHeight,
-            layoutAddLeft,
+            buttonAdd.layoutLeft,
             layoutAddWidth])
+        
+        DispatchQueue.main.asyncAfter(
+            deadline:DispatchTime.now() + kAfterInit)
+        { [weak self] in
+            
+            self?.buttonAdd.animateShow()
+        }
     }
     
-    override func layoutSubviews()
+    //MARK: actions
+    
+    func actionAdd(sender button:UIButton)
     {
-        let width:CGFloat = bounds.maxX
-        let height:CGFloat = bounds.maxY
-        let remainAddX:CGFloat = width - kButtonAddSize
-        let remainAddY:CGFloat = height - kButtonAddSize
-        let marginAddX:CGFloat = remainAddX / 2.0
-        let marginAddY:CGFloat = remainAddY / 2.0
-        layoutAddTop.constant = marginAddY
-        layoutAddLeft.constant = marginAddX
-        
-        super.layoutSubviews()
+        buttonAdd.animateHide()
     }
 }
--- looper/View/Camera/Filter/Blender/VCameraFilterBlenderOverlayListAdd.swift
+++ looper/View/Camera/Filter/Blender/VCameraFilterBlenderOverlayListAdd.swift
@@ -2,7 +2,10 @@ import UIKit
 
 class VCameraFilterBlenderOverlayListAdd:UIButton
 {
+    weak var layoutLeft:NSLayoutConstraint!
     private weak var image:UIImageView!
+    private let kAnimationImagesDuration:TimeInterval = 0.3
+    private let kAnimationDuration:TimeInterval = 1
     
     init()
     {
@@ -11,12 +14,22 @@ class VCameraFilterBlenderOverlayListAdd:UIButton
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor.clear
         
+        let animatingImages:[UIImage] = [
+        #imageLiteral(resourceName: "assetCameraFilterBlenderAdd1"),
+        #imageLiteral(resourceName: "assetCameraFilterBlenderAdd2"),
+        #imageLiteral(resourceName: "assetCameraFilterBlenderAdd3"),
+        #imageLiteral(resourceName: "assetCameraFilterBlenderAdd4"),
+        #imageLiteral(resourceName: "assetCameraFilterBlenderAdd5"),
+        #imageLiteral(resourceName: "assetCameraFilterBlenderAdd0")]
+        
         let image:UIImageView = UIImageView()
         image.isUserInteractionEnabled = false
         image.translatesAutoresizingMaskIntoConstraints = false
         image.clipsToBounds = true
         image.contentMode = UIViewContentMode.center
-        image.image = #imageLiteral(resourceName: "assetCameraFilterBlenderAdd")
+        image.image = #imageLiteral(resourceName: "assetCameraFilterBlenderAdd0")
+        image.animationImages = animatingImages
+        image.animationDuration = kAnimationImagesDuration
         self.image = image
         
         addSubview(image)
@@ -31,5 +44,70 @@ class VCameraFilterBlenderOverlayListAdd:UIButton
     required init?(coder:NSCoder)
     {
         fatalError()
+    }
+    
+    deinit
+    {
+        image.stopAnimating()
+    }
+    
+    //MARK: public
+    
+    func animateHide()
+    {
+        guard
+            
+            let width:CGFloat = superview?.bounds.maxX
+        
+        else
+        {
+            return
+        }
+        
+        image.startAnimating()
+        layoutLeft.constant = width
+        UIView.animate(
+            withDuration:kAnimationDuration,
+            animations:
+        { [weak self] in
+            
+            self?.superview?.layoutIfNeeded()
+            
+        })
+        { [weak self] (done:Bool) in
+        
+            self?.image.stopAnimating()
+        }
+    }
+    
+    func animateShow()
+    {
+        guard
+            
+            let width:CGFloat = superview?.bounds.maxX
+            
+        else
+        {
+            return
+        }
+        
+        let selfWidth:CGFloat = bounds.maxX
+        let remainLeft:CGFloat = width - selfWidth
+        let marginLeft:CGFloat = remainLeft / 2.0
+        
+        image.startAnimating()
+        layoutLeft.constant = marginLeft
+        UIView.animate(
+            withDuration:kAnimationDuration,
+            animations:
+        { [weak self] in
+            
+            self?.superview?.layoutIfNeeded()
+            
+        })
+        { [weak self] (done:Bool) in
+            
+            self?.image.stopAnimating()
+        }
     }
 }
