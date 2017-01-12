c8856219928fdebac0c2b07846b04471eea96970
vauxhall <zero.griffin@gmail.com>
2017-01-12 15:30:24 -0600

    view cell close

--- looper/View/Camera/More/VCameraMoreCellClose.swift
+++ looper/View/Camera/More/VCameraMoreCellClose.swift
@@ -1,9 +1,6 @@
-//
-//  VCameraMoreCellClose.swift
-//  looper
-//
-//  Created by zero on 1/12/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class VCameraMoreCellClose:UICollectionViewCell
+{
+    
+}
