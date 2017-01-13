91cd8afdb999ec3b6d41ba19ebb6a693b9b577d5
vauxhall <zero.griffin@gmail.com>
2017-01-13 15:15:03 -0600

    cell action options

---
+++ looper/View/Camera/More/VCameraMoreCellActionsOption.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class VCameraMoreCellActionsOption:UICollectionViewCell
+{
+    private weak var imageView:UIImageView!
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        backgroundColor = UIColor.clear
+        clipsToBounds = true
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        let constraintsImage:[NSLayoutConstraint] = NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+        
+        addConstraints(constraintsImage)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    //MARK: public
+    
+    func config()
+    {
+        
+    }
+}
