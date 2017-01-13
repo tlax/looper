862f0cbe3cb3e8f5ba304be0e569d04ae2c36b9c
vauxhall <zero.griffin@gmail.com>
2017-01-13 16:58:33 -0600

    no deselect, instead close more

--- looper/View/Camera/More/VCameraMoreCellActions.swift
+++ looper/View/Camera/More/VCameraMoreCellActions.swift
@@ -6,7 +6,6 @@ class VCameraMoreCellActions:VCameraMoreCell, UICollectionViewDelegate, UICollec
     private weak var collectionView:VCollection!
     private let kInterline:CGFloat = 1
     private let kCellWidth:CGFloat = 70
-    private let kDeselectTime:TimeInterval = 0.25
     
     override init(frame:CGRect)
     {
@@ -103,15 +102,6 @@ class VCameraMoreCellActions:VCameraMoreCell, UICollectionViewDelegate, UICollec
     {
         let item:MCameraMoreItemActionsOption = modelAtIndex(index:indexPath)
         item.selected(controller:controller)
-        
-        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).asyncAfter(
-            deadline:DispatchTime.now() + kDeselectTime)
-        { [weak collectionView] in
-            
-            collectionView?.selectItem(
-                at:indexPath,
-                animated:true,
-                scrollPosition:UICollectionViewScrollPosition())
-        }
+        controller?.viewMore.close()
     }
 }
