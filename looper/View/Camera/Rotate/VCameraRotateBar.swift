7bc9a1b55bd5f3485ec67ee7e4eb7be58fd51831
vauxhall <zero.griffin@gmail.com>
2017-01-13 20:26:59 -0600

    refactor constraints

--- looper/View/Camera/Rotate/VCameraRotateBar.swift
+++ looper/View/Camera/Rotate/VCameraRotateBar.swift
@@ -3,6 +3,7 @@ import UIKit
 class VCameraRotateBar:UIView
 {
     private weak var controller:CCameraRotate!
+    private let kBorderHeight:CGFloat = 1
     
     convenience init(controller:CCameraRotate)
     {
@@ -12,17 +13,24 @@ class VCameraRotateBar:UIView
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
         
-        let border:UIView = UIView()
-        border.isUserInteractionEnabled = false
-        border.backgroundColor = UIColor(white:0, alpha:0.1)
-        border.translatesAutoresizingMaskIntoConstraints = false
-        border.clipsToBounds = true
-        
+        let border:VBorder = VBorder(color:UIColor(white:0, alpha:0.1))
         addSubview(border)
         
-        let layoutBorderTop:NSLayoutConstraint = NSLayoutConstraint.topToTop(
+        let constraintsBorderHorizontal:[NSLayoutConstraint] = NSLayoutConstraint.equalsHorizontal(
             view:border,
             toView:self)
-        let layout
+        
+        let layoutBorderBottom:NSLayoutConstraint = NSLayoutConstraint.bottomToBottom(
+            view:border,
+            toView:self)
+        let layoutBorderHeight:NSLayoutConstraint = NSLayoutConstraint.height(
+            view:border,
+            constant:kBorderHeight)
+        
+        addConstraints(constraintsBorderHorizontal)
+        
+        addConstraints([
+            layoutBorderBottom,
+            layoutBorderHeight])
     }
 }
