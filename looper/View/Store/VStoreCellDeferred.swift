ad29632544883e63218a2077ee2d1969db73ac2f
vauxhall <zero.griffin@gmail.com>
2017-01-14 15:16:49 -0600

    refactor constraints

--- looper/View/Store/VStoreCellDeferred.swift
+++ looper/View/Store/VStoreCellDeferred.swift
@@ -17,11 +17,9 @@ class VStoreCellDeferred:VStoreCell
         
         addSubview(label)
         
-        let constraintsLabel:[NSLayoutConstraint] = NSLayoutConstraint.equals(
+        NSLayoutConstraint.equals(
             view:label,
             toView:self)
-        
-        addConstraints(constraintsLabel)
     }
     
     required init?(coder:NSCoder)
