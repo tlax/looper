c28b8a29bc8aa477db3c114cf8c2fb0b1e245666
vauxhall <zero.griffin@gmail.com>
2017-01-14 11:39:30 -0600

    view blur

--- looper/View/Camera/Rotate/VCameraRotate.swift
+++ looper/View/Camera/Rotate/VCameraRotate.swift
@@ -9,6 +9,13 @@ class VCameraRotate:VView
         super.init(controller:controller)
         backgroundColor = UIColor.clear
         self.controller = controller as? CCameraRotate
+        
+        let blur:UIBlurEffect = UIBlurEffect(style:UIBlurEffectStyle.extraLight)
+        let visualEffect:UIVisualEffectView = UIVisualEffectView(effect:blur)
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        visualEffect.clipsToBounds
+        
+        addSubview(visualEffect)
     }
     
     required init?(coder:NSCoder)
