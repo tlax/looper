e177cb1d2caf719c47555bc373d6e25c1bd05c93
vauxhall <zero.griffin@gmail.com>
2017-01-13 18:37:43 -0600

    view camera rotate

---
+++ looper/Controller/Camera/CCameraRotate.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class CCameraRotate:CController
+{
+    override func loadView()
+    {
+        
+    }
+}
---
+++ looper/View/Camera/Rotate/VCameraRotate.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class VCameraRotate:VView
+{
+    private weak var controller:CCameraRotate!
+}
