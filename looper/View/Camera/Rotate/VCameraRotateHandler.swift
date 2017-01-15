70b704ab66c14b05b362b04d7da28418f0e968b6
vauxhall <zero.griffin@gmail.com>
2017-01-15 15:28:37 -0600

    disp in circle

--- looper/View/Camera/Rotate/VCameraRotateHandler.swift
+++ looper/View/Camera/Rotate/VCameraRotateHandler.swift
@@ -3,19 +3,28 @@ import UIKit
 class VCameraRotateHandler:UIView
 {
     private var colorRad:UIColor
-    private var radius:CGFloat
+    private var colorDisp:UIColor
     private var centerArc:CGPoint
+    private var radius:CGFloat
+    private var dispRadius:CGFloat
+    private var dispInit:CGFloat
+    private var dispEnd:CGFloat
     private let kMargin:CGFloat = 50
     private let kZeroRad:CGFloat = -CGFloat(M_PI_2)
     private let k360Rad:CGFloat = CGFloat(M_PI + M_PI_2)
     private let kDeltaRad:CGFloat = 0.05
     private let kRadSize:CGFloat = 0.01
     private let kRadLineWidth:CGFloat = 10
+    private let kDispLineWidth:CGFloat = 12
     
     init()
     {
         colorRad = UIColor(white:0, alpha:0.6)
+        colorDisp = UIColor.genericLight
+        dispInit = kZeroRad
+        dispEnd = 2
         radius = 0
+        dispRadius = 0
         centerArc = CGPoint.zero
         
         super.init(frame:CGRect.zero)
@@ -36,6 +45,7 @@ class VCameraRotateHandler:UIView
         let centerY:CGFloat = bounds.midY
         let minSize:CGFloat = min(centerX, centerY)
         radius = minSize - kMargin
+        dispRadius = radius + kRadLineWidth
         centerArc = CGPoint(x:centerX, y:centerY)
         
         super.layoutSubviews()
@@ -52,6 +62,7 @@ class VCameraRotateHandler:UIView
             return
         }
         
+        context.setLineCap(CGLineCap.round)
         context.setLineWidth(kRadLineWidth)
         context.setStrokeColor(colorRad.cgColor)
         
@@ -73,5 +84,14 @@ class VCameraRotateHandler:UIView
             
             context.drawPath(using:CGPathDrawingMode.stroke)
         }
+        
+        context.setLineWidth(kDispLineWidth)
+        context.setStrokeColor(colorDisp.cgColor)
+        context.addArc(
+            center:centerArc,
+            radius:dispRadius,
+            startAngle:dispInit,
+            endAngle:dispEnd,
+            clockwise:false)
     }
 }
