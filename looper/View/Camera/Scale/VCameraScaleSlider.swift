3cae92edefee34583be2fbcdd61274b92f9d0c51
vauxhall <zero.griffin@gmail.com>
2017-01-16 15:20:28 -0600

    scale thumb

--- looper/View/Camera/Scale/VCameraScaleSlider.swift
+++ looper/View/Camera/Scale/VCameraScaleSlider.swift
@@ -2,12 +2,18 @@ import UIKit
 
 class VCameraScaleSlider:UIView
 {
+    private weak var labelPercent:UILabel!
+    private weak var labelSize:UILabel!
+    private weak var thumb:UIImageView!
+    
     init()
     {
         super.init(frame:CGRect.zero)
         clipsToBounds = true
         backgroundColor = UIColor.red
         translatesAutoresizingMaskIntoConstraints = false
+        
+        
     }
     
     required init?(coder:NSCoder)
