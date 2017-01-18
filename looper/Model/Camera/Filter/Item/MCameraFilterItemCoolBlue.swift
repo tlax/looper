8b96504c8db233e5a00d417cf0d1c46ffe4a00fc
vauxhall <zero.griffin@gmail.com>
2017-01-18 15:22:55 -0600

    wording

--- looper/Model/Camera/Filter/Item/MCameraFilterItemCoolBlue.swift
+++ looper/Model/Camera/Filter/Item/MCameraFilterItemCoolBlue.swift
@@ -5,7 +5,7 @@ class MCameraFilterItemCoolBlue:MCameraFilterItem
     init()
     {
         let title:String = NSLocalizedString("MCameraFilterItemCoolBlue_title", comment:"")
-        let viewTitle:String = NSLocalizedString("MCameraFilterItemCoolBlue_title", comment:"")
+        let viewTitle:String = NSLocalizedString("MCameraFilterItemCoolBlue_viewTitle", comment:"")
         let image:UIImage = #imageLiteral(resourceName: "assetFilterCoolBlue")
         
         super.init(title:title, viewTitle:viewTitle, image:image)
