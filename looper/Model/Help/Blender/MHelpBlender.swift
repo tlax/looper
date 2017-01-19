146a1b4ce49ac2fa67020df4397e1f03c0f222f9
vauxhall <zero.griffin@gmail.com>
2017-01-19 14:52:34 -0600

    removed item select

--- looper/Model/Help/Blender/MHelpBlender.swift
+++ looper/Model/Help/Blender/MHelpBlender.swift
@@ -7,14 +7,12 @@ class MHelpBlender:MHelp
         let itemAbout:MHelpItemBlenderAbout = MHelpItemBlenderAbout()
         let itemBase:MHelpItemBlenderBase = MHelpItemBlenderBase()
         let itemAdd:MHelpItemBlenderAdd = MHelpItemBlenderAdd()
-        let itemSelect:MHelpItemBlenderSelect = MHelpItemBlenderSelect()
         let itemDrag:MHelpItemBlenderDrag = MHelpItemBlenderDrag()
         
         let items:[MHelpItem] = [
             itemAbout,
             itemBase,
             itemAdd,
-            itemSelect,
             itemDrag
         ]
         
