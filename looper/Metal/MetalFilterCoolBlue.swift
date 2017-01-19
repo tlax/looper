1f9eb8c331076dae28006b5d1a66cc0698d5841d
vauxhall <zero.griffin@gmail.com>
2017-01-19 09:23:22 -0600

    metal filter cool blue

---
+++ looper/Metal/MetalFilterCoolBlue.swift
@@ -0,0 +1,28 @@
+import MetalPerformanceShaders
+
+class MetalFilterCoolBlue:MetalFilter
+{
+    private let kIndexBaseTexture:Int = 0
+    private let kIndexOverTexture:Int = 1
+    private let kIndexMapTexture:Int = 2
+    private let kIndexDestinationTexture:Int = 3
+    
+    //MARK: public
+    
+    func render(
+        overlayTexture:MTLTexture,
+        baseTexture:MTLTexture,
+        mapTexture:MTLTexture)
+    {
+        commandEncoder.setComputePipelineState(pipeline)
+        commandEncoder.setTexture(baseTexture, at:kIndexBaseTexture)
+        commandEncoder.setTexture(overlayTexture, at:kIndexOverTexture)
+        commandEncoder.setTexture(mapTexture, at:kIndexMapTexture)
+        commandEncoder.setTexture(baseTexture, at:kIndexDestinationTexture)
+        
+        commandEncoder.dispatchThreadgroups(
+            threadgroups,
+            threadsPerThreadgroup:threadgroupCounts)
+        commandEncoder.endEncoding()
+    }
+}
