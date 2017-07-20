06b9517c870aacca5aef9567a7d396e1c34b21e4
vauxhall <zero.griffin@gmail.com>
2017-07-20 10:22:12 -0500

    Create font functions

--- looper/Support/Extensions/ExtensionUIFont.swift
+++ looper/Support/Extensions/ExtensionUIFont.swift
@@ -2,9 +2,9 @@ import UIKit
 
 extension UIFont
 {
-    static let kFontRegular:String = "ArialMT"
-    static let kFontBold:String = "Arial-BoldMT"
-    static let kFontGame:String = "Pixeled"
+    static let kFontRegular:String = "ContinuumMedium"
+    static let kFontLight:String = "ContinuumLight"
+    static let kFontBold:String = "ContinuumBold"
     
     class func regular(size:CGFloat) -> UIFont
     {
@@ -20,9 +20,9 @@ extension UIFont
         return font
     }
     
-    class func game(size:CGFloat) -> UIFont
+    class func light(size:CGFloat) -> UIFont
     {
-        let font:UIFont = UIFont(name:kFontGame, size:size)!
+        let font:UIFont = UIFont(name:kFontLight, size:size)!
         
         return font
     }
