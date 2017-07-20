de4d81595bea3974fa4d5fdc575ad292e0073dd6
vauxhall <zero.griffin@gmail.com>
2017-07-20 11:03:59 -0500

    Create class view main

---
+++ looper/View/Abstract/Basic/ViewMain.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class ViewMain:View, ViewProtocol
+{
+    private(set) weak var controller:UIViewController!
+    weak var layoutLeft:NSLayoutConstraint!
+    weak var layoutRight:NSLayoutConstraint!
+    weak var layoutTop:NSLayoutConstraint!
+    weak var layoutBottom:NSLayoutConstraint!
+    weak var pushBackground:VPushBackground?
+    
+    required init(controller:UIViewController)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.white
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
