e3b3d5ac506a22bd3cd34c2cefd27e4105b07b47
vauxhall <zero.griffin@gmail.com>
2017-07-20 17:40:09 -0500

    Add bar to create

--- looper/View/Create/VCreate.swift
+++ looper/View/Create/VCreate.swift
@@ -2,5 +2,45 @@ import UIKit
 
 class VCreate:ViewMain
 {
+    private let kBarMaxHeight:CGFloat = 150
     
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        guard
+        
+            let controller:CCreate = controller as? CCreate
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CCreate)
+    {
+        let viewBar:VCreateBar = VCreateBar(controller:controller)
+        
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:kBarMaxHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
+    }
 }
--- looper/View/Create/VCreateBar.swift
+++ looper/View/Create/VCreateBar.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class VCreateBar:View<VCreate, MCreate, CCreate>
 {
+    required init(controller:CCreate)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor(red:0.02, green:0.03, blue:0.04, alpha:1)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
