2e8035ddaa18ec02896a364b662ad40431f56177
vauxhall <zero.griffin@gmail.com>
2017-07-20 18:53:32 -0500

    Add image to cell

--- looper/View/Create/VCreateMenuCell.swift
+++ looper/View/Create/VCreateMenuCell.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class VCreateMenuCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
     private let kCornerRadius:CGFloat = 6
     
     override init(frame:CGRect)
@@ -10,6 +11,19 @@ class VCreateMenuCell:UICollectionViewCell
         clipsToBounds = true
         backgroundColor = UIColor.white
         layer.cornerRadius = kCornerRadius
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
@@ -21,5 +35,6 @@ class VCreateMenuCell:UICollectionViewCell
     
     func config(model:MSourceProtocol)
     {
+        imageView.image = model.icon
     }
 }
