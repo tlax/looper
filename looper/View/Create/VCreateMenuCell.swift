c6dd937a5252322b7806b752176805808dca10cd
vauxhall <zero.griffin@gmail.com>
2017-07-20 23:51:52 -0500

    Add label title

--- looper/View/Create/VCreateMenuCell.swift
+++ looper/View/Create/VCreateMenuCell.swift
@@ -3,7 +3,9 @@ import UIKit
 class VCreateMenuCell:UICollectionViewCell
 {
     private weak var imageView:UIImageView!
+    private weak var labelTitle:UILabel!
     private let kCornerRadius:CGFloat = 6
+    private let kTitleHeight:CGFloat = 20
     
     override init(frame:CGRect)
     {
@@ -19,10 +21,30 @@ class VCreateMenuCell:UICollectionViewCell
         imageView.contentMode = UIViewContentMode.center
         self.imageView = imageView
         
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.textAlignment = NSTextAlignment.center
+        labelTitle.font = UIFont.regular(size:11)
+        labelTitle.textColor = UIColor.black
+        self.labelTitle = labelTitle
+        
         addSubview(imageView)
+        addSubview(labelTitle)
         
         NSLayoutConstraint.equals(
             view:imageView,
+            toView:self)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:labelTitle,
+            constant:kTitleHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:labelTitle,
             toView:self)
     }
     
