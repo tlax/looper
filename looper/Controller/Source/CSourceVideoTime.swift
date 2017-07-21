7b639da64fe555657928fe93c1a90cf022ab7811
vauxhall <zero.griffin@gmail.com>
2017-07-21 16:24:47 -0500

    Create controller video time

--- looper/Controller/Source/CSourceVideoTime.swift
+++ looper/Controller/Source/CSourceVideoTime.swift
@@ -1,9 +1,6 @@
-//
-//  CSourceVideoTime.swift
-//  looper
-//
-//  Created by zero on 7/21/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class CSourceVideoTime:Controller<VSourceVideoTime, MSourceVideoTime>
+{
+    
+}
