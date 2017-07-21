f070ad4c588d1a3a75c0423f4b09f8e2844079db
vauxhall <zero.griffin@gmail.com>
2017-07-21 01:12:49 -0500

    Allow gesture on video and image

--- looper/View/SourceImage/VSourceImage.swift
+++ looper/View/SourceImage/VSourceImage.swift
@@ -2,4 +2,13 @@ import UIKit
 
 class VSourceImage:ViewMain
 {
+    private let kPanBack:Bool = true
+    
+    override var panBack:Bool
+    {
+        get
+        {
+            return kPanBack
+        }
+    }
 }
--- looper/View/SourceVideo/VSourceVideo.swift
+++ looper/View/SourceVideo/VSourceVideo.swift
@@ -2,4 +2,13 @@ import UIKit
 
 class VSourceVideo:ViewMain
 {
+    private let kPanBack:Bool = true
+    
+    override var panBack:Bool
+    {
+        get
+        {
+            return kPanBack
+        }
+    }
 }
