3c9fe8d341df47596fa7494f49426c5cbd2dcc34
vauxhall <zero.griffin@gmail.com>
2017-07-21 02:29:40 -0500

    Update spinner

--- looper/View/Basic/VSpinner.swift
+++ looper/View/Basic/VSpinner.swift
@@ -10,7 +10,23 @@ class VSpinner:UIImageView
         
         let images:[UIImage] = [
             #imageLiteral(resourceName: "assetSpinner00"),
-        ]
+            #imageLiteral(resourceName: "assetSpinner01"),
+            #imageLiteral(resourceName: "assetSpinner02"),
+            #imageLiteral(resourceName: "assetSpinner03"),
+            #imageLiteral(resourceName: "assetSpinner04"),
+            #imageLiteral(resourceName: "assetSpinner05"),
+            #imageLiteral(resourceName: "assetSpinner06"),
+            #imageLiteral(resourceName: "assetSpinner07"),
+            #imageLiteral(resourceName: "assetSpinner08"),
+            #imageLiteral(resourceName: "assetSpinner09"),
+            #imageLiteral(resourceName: "assetSpinner10"),
+            #imageLiteral(resourceName: "assetSpinner11"),
+            #imageLiteral(resourceName: "assetSpinner12"),
+            #imageLiteral(resourceName: "assetSpinner13"),
+            #imageLiteral(resourceName: "assetSpinner14"),
+            #imageLiteral(resourceName: "assetSpinner15"),
+            #imageLiteral(resourceName: "assetSpinner16"),
+            #imageLiteral(resourceName: "assetSpinner17")]
         
         isUserInteractionEnabled = false
         translatesAutoresizingMaskIntoConstraints = false
