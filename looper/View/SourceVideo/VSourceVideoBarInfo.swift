6c24090af4d4a41bb64b64501a4aa3a4ec6bdd9f
vauxhall <zero.griffin@gmail.com>
2017-07-21 15:31:15 -0500

    Add cells to info

--- looper/View/SourceVideo/VSourceVideoBarInfo.swift
+++ looper/View/SourceVideo/VSourceVideoBarInfo.swift
@@ -2,9 +2,62 @@ import UIKit
 
 class VSourceVideoBarInfo:View<VSourceVideo, MSourceVideo, CSourceVideo>
 {
+    private weak var viewCellWidth:VSourceVideoBarInfoCell!
+    private weak var viewCellHeight:VSourceVideoBarInfoCell!
+    private weak var viewCellDuration:VSourceVideoBarInfoCell!
+    private let kCellHeight:CGFloat = 40
+    private let kCellTop:CGFloat = 120
+    
     required init(controller:CSourceVideo)
     {
         super.init(controller:controller)
+        
+        let viewCellWidth:VSourceVideoBarInfoCell = VSourceVideoBarInfoCell(controller:controller)
+        viewCellWidth.changeTitle(title:String.localizedView(key:"VSourceVideoBarInfo_titleWidth"))
+        self.viewCellWidth = viewCellWidth
+        
+        let viewCellHeight:VSourceVideoBarInfoCell = VSourceVideoBarInfoCell(controller:controller)
+        viewCellHeight.changeTitle(title:String.localizedView(key:"VSourceVideoBarInfo_titleHeight"))
+        self.viewCellHeight = viewCellHeight
+        
+        let viewCellDuration:VSourceVideoBarInfoCell = VSourceVideoBarInfoCell(controller:controller)
+        viewCellDuration.changeTitle(title:String.localizedView(key:"VSourceVideoBarInfo_titleDuration"))
+        self.viewCellDuration = viewCellDuration
+        
+        addSubview(viewCellWidth)
+        addSubview(viewCellHeight)
+        addSubview(viewCellDuration)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewCellWidth,
+            toView:self,
+            constant:kCellTop)
+        NSLayoutConstraint.height(
+            view:viewCellWidth,
+            constant:kCellHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewCellWidth,
+            toView:self)
+        
+        NSLayoutConstraint.topToBottom(
+            view:viewCellHeight,
+            toView:viewCellWidth)
+        NSLayoutConstraint.height(
+            view:viewCellHeight,
+            constant:kCellHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewCellHeight,
+            toView:self)
+        
+        NSLayoutConstraint.topToBottom(
+            view:viewCellDuration,
+            toView:viewCellHeight)
+        NSLayoutConstraint.height(
+            view:viewCellDuration,
+            constant:kCellHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewCellDuration,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
