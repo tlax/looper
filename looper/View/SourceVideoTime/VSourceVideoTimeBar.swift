b7f64df545f596d0ed95057d6a09c6b61fbef262
vauxhall <zero.griffin@gmail.com>
2017-07-21 18:27:41 -0500

    Add background image

--- looper/View/SourceVideoTime/VSourceVideoTimeBar.swift
+++ looper/View/SourceVideoTime/VSourceVideoTimeBar.swift
@@ -5,7 +5,20 @@ View<VSourceVideoTime, MSourceVideoTime, CSourceVideoTime>
 {
     required init(controller:CSourceVideoTime)
     {
-        super.init(controller:controler)
+        super.init(controller:controller)
+        
+        let backgroundImage:UIImageView = UIImageView()
+        backgroundImage.isUserInteractionEnabled = false
+        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImage.clipsToBounds = true
+        backgroundImage.contentMode = UIViewContentMode.scaleAspectFill
+        backgroundImage.image = controller.model.item.image
+        
+        addSubview(backgroundImage)
+        
+        NSLayoutConstraint.equals(
+            view:backgroundImage,
+            toView:self)
     }
     
     required init?(coder: NSCoder)
