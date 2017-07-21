49d7e47fca216eef88972ce5c1627b8718331953
vauxhall <zero.griffin@gmail.com>
2017-07-21 18:30:14 -0500

    Add view bar

--- looper/View/SourceVideoTime/VSourceVideoTime.swift
+++ looper/View/SourceVideoTime/VSourceVideoTime.swift
@@ -2,5 +2,47 @@ import UIKit
 
 class VSourceVideoTime:ViewMain
 {
+    private weak var layoutBarHeight:NSLayoutConstraint!
+    private let kBarMaxHeight:CGFloat = 200
     
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        guard
+        
+            let controller:CSourceVideoTime = self.controller as? CSourceVideoTime
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CSourceVideoTime)
+    {
+        let viewBar:VSourceVideoTimeBar = VSourceVideoTimeBar(
+            controller:controller)
+        
+        addSubview(viewBar)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        layoutBarHeight = NSLayoutConstraint.height(
+            view:viewBar,
+            constant:kBarMaxHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
+    }
 }
