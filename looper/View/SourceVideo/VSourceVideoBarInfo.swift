74e561b7c03fb120a1f9ad891c6dec3ae97ef0d1
vauxhall <zero.griffin@gmail.com>
2017-07-21 15:18:43 -0500

    Create info cell

--- looper/View/SourceVideo/VSourceVideoBarInfo.swift
+++ looper/View/SourceVideo/VSourceVideoBarInfo.swift
@@ -2,5 +2,20 @@ import UIKit
 
 class VSourceVideoBarInfo:View<VSourceVideo, MSourceVideo, CSourceVideo>
 {
+    required init(controller:CSourceVideo)
+    {
+        super.init(controller:controller)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config(model:MSourceVideoItem)
+    {
+        
+    }
 }
