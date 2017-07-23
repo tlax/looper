47a409399d2315613c25a5ce796cb501f97c9807
vauxhall <zero.griffin@gmail.com>
2017-07-23 06:35:15 -0500

    Add blur in new

--- looper/Controller/New/CNew.swift
+++ looper/Controller/New/CNew.swift
@@ -2,5 +2,19 @@ import Foundation
 
 class CNew:Controller<VNew, MNew>
 {
+    //MARK: public
     
+    func cancel()
+    {
+        guard
+            
+            let parent:ControllerParent = parent as? ControllerParent
+            
+        else
+        {
+            return
+        }
+        
+        parent.dismissAnimateOver(completion:nil)
+    }
 }
--- looper/View/New/VNew.swift
+++ looper/View/New/VNew.swift
@@ -2,5 +2,67 @@ import UIKit
 
 class VNew:ViewMain
 {
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.clear
+        
+        guard
+        
+            let controller:CNew = controller as? CNew
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: actions
+    
+    func actionCancel(sender button:UIButton)
+    {
+        guard
+        
+            let controller:CNew = self.controller as? CNew
+        
+        else
+        {
+            return
+        }
+        
+        controller.cancel()
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CNew)
+    {
+        let blur:VBlur = VBlur.light()
+        
+        let button:UIButton = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.addTarget(
+            self,
+            action:#selector(actionCancel(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
+        addSubview(blur)
+        addSubview(button)
+        
+        NSLayoutConstraint.equals(
+            view:blur,
+            toView:self)
+        
+        NSLayoutConstraint.equals(
+            view:button,
+            toView:self)
+    }
 }
