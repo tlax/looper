478e410a4a021d5bb4a31a8f246724509ee4e85a
vauxhall <zero.griffin@gmail.com>
2017-07-22 20:53:07 -0500

    Create controller import

--- looper/Controller/Source/CSourceVideoImport.swift
+++ looper/Controller/Source/CSourceVideoImport.swift
@@ -1,9 +1,6 @@
-//
-//  CSourceVideoImport.swift
-//  looper
-//
-//  Created by zero on 7/22/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class CSourceVideoImport:Controller<VSourceVideoImport, MSourceVideoImport>
+{
+    
+}
