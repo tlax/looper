0a2fb5a1efda2e1733799eb48271980d19843e64
vauxhall <zero.griffin@gmail.com>
2017-07-24 12:39:10 -0500

    Factory views

--- looper/View/Edit/VEdit.swift
+++ looper/View/Edit/VEdit.swift
@@ -2,4 +2,31 @@ import UIKit
 
 class VEdit:ViewMain
 {
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        guard
+        
+            let controller:CEdit = controller as? CEdit
+        
+        else
+        {
+            return
+        }
+        
+        factoryViews(controller:controller)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryViews(controller:CEdit)
+    {
+        
+    }
 }
