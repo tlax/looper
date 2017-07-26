fa0d822d9ef522db93e09fe5173b23d246e346f5
vauxhall <zero.griffin@gmail.com>
2017-07-26 13:48:20 -0500

    Export duration

--- looper/Model/EditDuration/MEditDuration.swift
+++ looper/Model/EditDuration/MEditDuration.swift
@@ -3,6 +3,47 @@ import Foundation
 class MEditDuration:Model
 {
     private(set) weak var edit:MEdit!
+    var selectedMinute:Int
+    var selectedSecond:Int
+    let minutes:[MEditDurationItem]
+    let seconds:[MEditDurationItem]
+    private let kSecondsPerMinute:Int = 60
+    private let kMinSeconds:Int = 1
+    
+    required init()
+    {
+        minutes = MEditDuration.factoryMinutes()
+        seconds = MEditDuration.factorySeconds()
+        selectedMinute = 0
+        selectedSecond = 0
+        
+        super.init()
+    }
+    
+    //MARK: private
+    
+    private func importDuration()
+    {
+        
+    }
+    
+    private func exportDuration()
+    {
+        let minute:MEditDurationItem = minutes[selectedMinute]
+        let second:MEditDurationItem = seconds[selectedSecond]
+        let minuteValue:Int = minute.value
+        let secondValue:Int = second.value
+        let minuteSeconds:Int = minuteValue * kSecondsPerMinute
+        var totalSeconds:Int = minuteSeconds + secondValue
+        
+        if totalSeconds < kMinSeconds
+        {
+            totalSeconds = kMinSeconds
+        }
+        
+        let duration:TimeInterval = TimeInterval(totalSeconds)
+        edit.sequence?.duration = duration
+    }
     
     //MARK: public
     
--- looper/Model/EditDuration/MEditDurationFactory.swift
+++ looper/Model/EditDuration/MEditDurationFactory.swift
@@ -2,5 +2,36 @@ import Foundation
 
 extension MEditDuration
 {
+    private static let kMinMinutes:Int = 0
+    private static let kMaxMinutes:Int = 30
+    private static let kMinSeconds:Int = 0
+    private static let kMaxSeconds:Int = 59
     
+    class func factoryMinutes() -> [MEditDurationItem]
+    {
+        var items:[MEditDurationItem] = []
+        
+        for minute:Int in kMinMinutes ..< kMaxMinutes
+        {
+            let item:MEditDurationItem = MEditDurationItem(
+                value:minute)
+            items.append(item)
+        }
+        
+        return items
+    }
+    
+    class func factorySeconds() -> [MEditDurationItem]
+    {
+        var items:[MEditDurationItem] = []
+        
+        for second:Int in kMinSeconds ..< kMaxSeconds
+        {
+            let item:MEditDurationItem = MEditDurationItem(
+                value:second)
+            items.append(item)
+        }
+        
+        return items
+    }
 }
