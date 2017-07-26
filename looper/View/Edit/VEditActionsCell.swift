8e1cf0ec7512ac079ef5610a3c66ac55bb7453ef
vauxhall <zero.griffin@gmail.com>
2017-07-26 00:34:44 -0500

    Layout action cell

--- looper/View/Edit/VEditActionsCell.swift
+++ looper/View/Edit/VEditActionsCell.swift
@@ -2,5 +2,69 @@ import UIKit
 
 class VEditActionsCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
+    private let kAlphaSelected:CGFloat = 0.2
+    private let kAlphaNotSelected:CGFloat = 1
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            alpha = kAlphaSelected
+        }
+        else
+        {
+            alpha = kAlphaNotSelected
+        }
+    }
+    
+    //MARK: public
+    
+    func config(model:MEditActionProtocol)
+    {
+        imageView.image = model.image
+    }
 }
