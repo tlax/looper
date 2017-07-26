68435506f8ee38bdf9845aa3f0488fc42b715528
vauxhall <zero.griffin@gmail.com>
2017-07-26 00:00:58 -0500

    Add cancel button

--- looper/View/Edit/VEdit.swift
+++ looper/View/Edit/VEdit.swift
@@ -3,9 +3,15 @@ import UIKit
 class VEdit:ViewMain
 {
     private weak var layoutSaveLeft:NSLayoutConstraint!
+    private weak var layoutCancelLeft:NSLayoutConstraint!
     private let kSaveWidth:CGFloat = 195
     private let kSaveTop:CGFloat = -32
     private let kSaveHeight:CGFloat = 64
+    private let kCancelWidth:CGFloat = 140
+    private let kCancelBottom:CGFloat = -20
+    private let kCancelHeight:CGFloat = 40
+    private let kActionsHeight:CGFloat = 60
+    private let kActionsBottom:CGFloat = 50
     
     private weak var layoutBarHeight:NSLayoutConstraint!
     
@@ -35,10 +41,20 @@ class VEdit:ViewMain
         let width:CGFloat = bounds.width
         let remainSave:CGFloat = width - kSaveWidth
         let saveMarginLeft:CGFloat = remainSave / 2.0
+        let remainCancel:CGFloat = width - kCancelWidth
+        let cancelMarginLeft:CGFloat = remainCancel / 2.0
         layoutBarHeight.constant = width
         layoutSaveLeft.constant = saveMarginLeft
+        layoutCancelLeft.constant = cancelMarginLeft
         
         super.layoutSubviews()
+    }
+    
+    //MARK: actions
+    
+    func actionCancel(sender button:UIButton)
+    {
+        
     }
     
     //MARK: private
@@ -49,7 +65,25 @@ class VEdit:ViewMain
         
         let viewSave:VEditSave = VEditSave(controller:controller)
         
+        let buttonCancel:UIButton = UIButton()
+        buttonCancel.translatesAutoresizingMaskIntoConstraints = false
+        buttonCancel.setTitleColor(
+            UIColor.colourBackgroundDark,
+            for:UIControlState.normal)
+        buttonCancel.setTitleColor(
+            UIColor.colourBackgroundGray,
+            for:UIControlState.highlighted)
+        buttonCancel.setTitle(
+            String.localizedView(key:"VEdit_buttonCancel"),
+            for:UIControlState.normal)
+        buttonCancel.titleLabel!.font = UIFont.regular(size:14)
+        buttonCancel.addTarget(
+            self,
+            action:#selector(actionCancel(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
         addSubview(viewBar)
+        addSubview(buttonCancel)
         addSubview(viewSave)
         
         NSLayoutConstraint.topToTop(
@@ -73,6 +107,20 @@ class VEdit:ViewMain
             constant:kSaveWidth)
         layoutSaveLeft = NSLayoutConstraint.leftToLeft(
             view:viewSave,
+            toView:self)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:buttonCancel,
+            toView:self,
+            constant:kCancelBottom)
+        NSLayoutConstraint.height(
+            view:buttonCancel,
+            constant:kCancelHeight)
+        NSLayoutConstraint.width(
+            view:buttonCancel,
+            constant:kCancelWidth)
+        layoutCancelLeft = NSLayoutConstraint.leftToLeft(
+            view:buttonCancel,
             toView:self)
     }
 }
