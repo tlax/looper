966a60b23b97ebfcb112aba6ff64fbee656259aa
vauxhall <zero.griffin@gmail.com>
2017-07-26 15:05:29 -0500

    Layout collection view

--- looper/View/EditDuration/VEditDurationCell.swift
+++ looper/View/EditDuration/VEditDurationCell.swift
@@ -2,5 +2,108 @@ import UIKit
 
 class VEditDurationCell:UICollectionViewCell
 {
+    private weak var label:UILabel!
+    private weak var viewCircle:UIView!
+    private weak var layoutCircleLeft:NSLayoutConstraint!
+    private weak var layoutCircleTop:NSLayoutConstraint!
+    private let kCircleSize:CGFloat = 38
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        
+        let viewCircle:UIView = UIView()
+        viewCircle.isUserInteractionEnabled = false
+        viewCircle.translatesAutoresizingMaskIntoConstraints = false
+        viewCircle.backgroundColor = UIColor(red:1, green:0.3, blue:0, alpha:1)
+        viewCircle.clipsToBounds = true
+        viewCircle.layer.cornerRadius = kCircleSize / 2.0
+        self.viewCircle = viewCircle
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.backgroundColor = UIColor.clear
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = NSTextAlignment.center
+        label.font = UIFont.medium(size:12)
+        self.label = label
+        
+        addSubview(viewCircle)
+        addSubview(label)
+        
+        NSLayoutConstraint.equals(
+            view:label,
+            toView:self)
+        
+        layoutCircleTop = NSLayoutConstraint.topToTop(
+            view:viewCircle,
+            toView:self)
+        layoutCircleLeft = NSLayoutConstraint.leftToLeft(
+            view:viewCircle,
+            toView:self)
+        NSLayoutConstraint.size(
+            view:viewCircle,
+            constant:kCircleSize)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func layoutSubviews()
+    {
+        let width:CGFloat = bounds.width
+        let height:CGFloat = bounds.height
+        let remainWidth:CGFloat = width - kCircleSize
+        let remainHeight:CGFloat = height - kCircleSize
+        let marginLeft:CGFloat = remainWidth / 2.0
+        let marginTop:CGFloat = remainHeight / 2.0
+        layoutCircleLeft.constant = marginLeft
+        layoutCircleTop.constant = marginTop
+        
+        super.layoutSubviews()
+    }
+    
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            viewCircle.isHidden = false
+            label.textColor = UIColor.white
+        }
+        else
+        {
+            viewCircle.isHidden = true
+            label.textColor = UIColor.colourBackgroundDark.withAlphaComponent(0.5)
+        }
+    }
+    
+    //MARK: public
+    
+    func config(model:MEditDurationItem)
+    {
+        label.text = "\(model.value)"
+        hover()
+    }
 }
--- looper/View/EditDuration/VEditDurationMinutes.swift
+++ looper/View/EditDuration/VEditDurationMinutes.swift
@@ -6,5 +6,72 @@ class VEditDurationMinutes:VCollection<
     CEditDuration,
     VEditDurationCell>
 {
+    private var cellSize:CGSize?
+    private let kCellWidth:CGFloat = 40
     
+    required init(controller:CEditDuration)
+    {
+        super.init(controller:controller)
+        collectionView.alwaysBounceHorizontal = true
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+        }
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        layout collectionViewLayout:UICollectionViewLayout,
+        sizeForItemAt indexPath:IndexPath) -> CGSize
+    {
+        guard
+        
+            let cellSize:CGSize = self.cellSize
+        
+        else
+        {
+            let height:CGFloat = collectionView.bounds.height
+            let cellSize:CGSize = CGSize(width:kCellWidth, height:height)
+            self.cellSize = cellSize
+            
+            return cellSize
+        }
+        
+        return cellSize
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        numberOfItemsInSection section:Int) -> Int
+    {
+        let count:Int = controller.model.minutes.count
+        
+        return count
+    }
+    
+    override func collectionView(
+        _ collectionView:UICollectionView,
+        cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
+    {
+        let item:MEditDurationItem = modelAtIndex(index:indexPath)
+        let cell:VEditDurationCell = cellAtIndex(indexPath:indexPath)
+        cell.config(model:item)
+        
+        return cell
+    }
+    
+    //MARK: private
+    
+    private func modelAtIndex(index:IndexPath) -> MEditDurationItem
+    {
+        let item:MEditDurationItem = controller.model.minutes[index.item]
+        
+        return item
+    }
 }
