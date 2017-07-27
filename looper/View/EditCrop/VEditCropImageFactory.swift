19f500d24e2b4589401196555823e0e2c3b0f0a2
vauxhall <zero.griffin@gmail.com>
2017-07-27 04:31:34 -0500

    Create extension layout

--- looper/View/EditCrop/VEditCropImageFactory.swift
+++ looper/View/EditCrop/VEditCropImageFactory.swift
@@ -2,4 +2,5 @@ import Foundation
 
 extension VEditCropImage
 {
+    
 }
--- looper/View/EditCrop/VEditCropImageLayout.swift
+++ looper/View/EditCrop/VEditCropImageLayout.swift
@@ -1,9 +1,6 @@
-//
-//  VEditCropImageLayout.swift
-//  looper
-//
-//  Created by zero on 7/27/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
-
 import Foundation
+
+extension VEditCropImage
+{
+    
+}
