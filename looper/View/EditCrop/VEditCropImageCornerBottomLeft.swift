51142d6f4feb779abab9aa0c4359ec16f44b8c17
vauxhall <zero.griffin@gmail.com>
2017-07-27 06:27:57 -0500

    Draw corner bottom left

--- looper/View/EditCrop/VEditCropImageCornerBottomLeft.swift
+++ looper/View/EditCrop/VEditCropImageCornerBottomLeft.swift
@@ -2,5 +2,25 @@ import UIKit
 
 class VEditCropImageCornerBottomLeft:VEditCropImageCorner
 {
-    
+    override func drawWithContext(
+        context:CGContext,
+        width:CGFloat,
+        height:CGFloat)
+    {
+        let maxY:CGFloat = height - lineWidth_2
+        
+        let initialPoint:CGPoint = CGPoint(
+            x:lineWidth_2,
+            y:0)
+        let middlePoint:CGPoint = CGPoint(
+            x:lineWidth_2,
+            y:maxY)
+        let endPoint:CGPoint = CGPoint(
+            x:width,
+            y:maxY)
+        
+        context.move(to:initialPoint)
+        context.addLine(to:middlePoint)
+        context.addLine(to:endPoint)
+    }
 }
