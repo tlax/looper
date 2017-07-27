8187ca14c72bc3dddb400b800908b3fad079ca7d
vauxhall <zero.griffin@gmail.com>
2017-07-26 19:28:38 -0500

    Add button reset

--- looper/View/EditCrop/VEditCrop.swift
+++ looper/View/EditCrop/VEditCrop.swift
@@ -2,11 +2,15 @@ import UIKit
 
 class VEditCrop:ViewMain
 {
-    private weak var layoutImageHeight:NSLayoutConstraint!
     private weak var layoutOkayLeft:NSLayoutConstraint!
+    private weak var layoutResetLeft:NSLayoutConstraint!
     private let kOkayWidth:CGFloat = 195
     private let kOkayBottom:CGFloat = -20
     private let kOkayHeight:CGFloat = 64
+    private let kResetBottom:CGFloat = -20
+    private let kResetHeight:CGFloat = 34
+    private let kResetWidth:CGFloat = 120
+    private let kImageBottom:CGFloat = -60
     
     required init(controller:UIViewController)
     {
@@ -34,10 +38,19 @@ class VEditCrop:ViewMain
         let width:CGFloat = bounds.width
         let remainOkay:CGFloat = width - kOkayWidth
         let okayMarginLeft:CGFloat = remainOkay / 2.0
-        layoutImageHeight.constant = width
+        let remainReset:CGFloat = width - kResetWidth
+        let resetMarginLeft:CGFloat = remainReset / 2.0
         layoutOkayLeft.constant = okayMarginLeft
+        layoutResetLeft.constant = resetMarginLeft
         
         super.layoutSubviews()
+    }
+    
+    //MARK: actions
+    
+    func actionReset(sender button:UIButton)
+    {
+        
     }
     
     //MARK: private
@@ -48,16 +61,50 @@ class VEditCrop:ViewMain
         
         let viewOkay:VEditCropOkay = VEditCropOkay(controller:controller)
         
+        let buttonReset:UIButton = UIButton()
+        buttonReset.translatesAutoresizingMaskIntoConstraints = false
+        buttonReset.setTitleColor(
+            UIColor.colourGradientDark.withAlphaComponent(0.6),
+            for:UIControlState.normal)
+        buttonReset.setTitleColor(
+            UIColor.colourBackgroundGray,
+            for:UIControlState.highlighted)
+        buttonReset.setTitle(
+            String.localizedView(key:"VEditCrop_buttonReset"),
+            for:UIControlState.normal)
+        buttonReset.titleLabel!.font = UIFont.medium(size:15)
+        buttonReset.addTarget(
+            self,
+            action:#selector(actionReset(sender:)),
+            for:UIControlEvents.touchUpInside)
+        
         addSubview(viewImage)
+        addSubview(buttonReset)
         addSubview(viewOkay)
         
         NSLayoutConstraint.topToTop(
             view:viewImage,
             toView:self)
-        layoutImageHeight = NSLayoutConstraint.height(
-            view:viewImage)
+        NSLayoutConstraint.bottomToTop(
+            view:viewImage,
+            toView:buttonReset,
+            constant:kImageBottom)
         NSLayoutConstraint.equalsHorizontal(
             view:viewImage,
+            toView:self)
+        
+        NSLayoutConstraint.bottomToTop(
+            view:buttonReset,
+            toView:viewOkay,
+            constant:kResetBottom)
+        NSLayoutConstraint.height(
+            view:buttonReset,
+            constant:kResetHeight)
+        NSLayoutConstraint.width(
+            view:buttonReset,
+            constant:kResetWidth)
+        layoutResetLeft = NSLayoutConstraint.leftToLeft(
+            view:buttonReset,
             toView:self)
         
         NSLayoutConstraint.bottomToBottom(
--- looper/View/EditCrop/VEditCropImage.swift
+++ looper/View/EditCrop/VEditCropImage.swift
@@ -2,14 +2,100 @@ import UIKit
 
 class VEditCropImage:View<VEditCrop, MEditCrop, CEditCrop>
 {
+    private weak var layoutImageLeft:NSLayoutConstraint!
+    private weak var layoutImageRight:NSLayoutConstraint!
+    private weak var layoutImageTop:NSLayoutConstraint!
+    private weak var layoutImageBottom:NSLayoutConstraint!
+    private var image:UIImage?
+    private let imageMargin2:CGFloat
+    private let kImageMargin:CGFloat = 30
+    private let kImageBorderWidth:CGFloat = 1
+    
     required init(controller:CEditCrop)
     {
+        imageMargin2 = kImageMargin + kImageMargin
+        
         super.init(controller:controller)
         backgroundColor = UIColor.black
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
+        imageView.layer.borderWidth = kImageBorderWidth
+        imageView.layer.borderColor = UIColor(white:1, alpha:0.2).cgColor
+        
+        addSubview(imageView)
+        
+        layoutImageTop = NSLayoutConstraint.topToTop(
+            view:imageView,
+            toView:self)
+        layoutImageBottom = NSLayoutConstraint.bottomToBottom(
+            view:imageView,
+            toView:self)
+        layoutImageLeft = NSLayoutConstraint.leftToLeft(
+            view:imageView,
+            toView:self)
+        layoutImageRight = NSLayoutConstraint.rightToRight(
+            view:imageView,
+            toView:self)
+        
+        guard
+            
+            let cgImage:CGImage = controller.model.edit.sequence?.items.first?.image
+            
+        else
+        {
+            return
+        }
+        
+        image = UIImage(cgImage:cgImage)
+        imageView.image = image
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: private
+    
+    private func constraintImage()
+    {
+        guard
+        
+            let image:UIImage = self.image
+        
+        else
+        {
+            return
+        }
+        
+        let canvasSize:CGFloat = bounds.width
+        let usableCanvas:CGFloat = canvasSize - imageMargin2
+        let width:CGFloat = image.size.width
+        let height:CGFloat = image.size.height
+        let maxSize:CGFloat = max(width, height)
+        let ratio:CGFloat = maxSize / usableCanvas
+        let usableWidth:CGFloat = width / ratio
+        let usableHeight:CGFloat = height / ratio
+        let remainWidth:CGFloat = canvasSize - usableWidth
+        let remainHeight:CGFloat = canvasSize - usableHeight
+        let marginWidth:CGFloat = remainWidth / 2.0
+        let marginHeight:CGFloat = remainHeight / 2.0
+        
+        layoutImageRight.constant = -marginWidth
+        layoutImageLeft.constant = marginWidth
+        layoutImageTop.constant = marginHeight
+        layoutImageBottom.constant = -marginHeight
+    }
+    
+    override func layoutSubviews()
+    {
+        print("layout")
+        constraintImage()
+        
+        super.layoutSubviews()
     }
 }
